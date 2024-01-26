2812d51f5f39f8469795064add373e0a86b1d7d1
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-01-26 22:59:53 +0900

    Add some tests.

--- NumFlatTest/VectorTests.cs
+++ NumFlatTest/VectorTests.cs
@@ -9,14 +9,29 @@ namespace NumFlatTest
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
         public void New(int count)
         {
             var vector = new Vec<int>(count);
             Assert.That(vector.Count == count);
             Assert.That(vector.Stride == 1);
             Assert.IsTrue(vector.Memory.Length == count);
+        }
+
+        [TestCase(1, 1, new int[] { 1 })]
+        [TestCase(3, 1, new int[] { 1, 2, 3 })]
+        [TestCase(3, 2, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(3, 3, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        [TestCase(2, 3, new int[] { 1, 2, 3, 4 })]
+        [TestCase(1, 3, new int[] { 1 })]
+        public void Stride(int count, int stride, int[] memory)
+        {
+            var vector = new Vec<int>(count, stride, memory);
+            var memoryIndex = 0;
+            for (var vectorIndex = 0; vectorIndex < count; vectorIndex++)
+            {
+                Assert.That(vector[vectorIndex] == memory[memoryIndex]);
+                memoryIndex += stride;
+            }
         }
     }
 }
