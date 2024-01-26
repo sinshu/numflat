6eb624712a4d0eb1b2ee51f1ce4a2aaa573de664
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-01-26 22:23:11 +0900

    First commit.

---
+++ NumFlatTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace NumFlatTest
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ clean.bat
@@ -0,0 +1,7 @@
+rmdir /s /q "NumFlat\bin"
+rmdir /s /q "NumFlat\obj"
+rmdir /s /q "NumFlatTest\bin"
+rmdir /s /q "NumFlatTest\obj"
+rmdir /s /q "Workspace\bin"
+rmdir /s /q "Workspace\obj"
+pause
