fef0f16b8ae9e1bb790ff4ee17f0f46b89355853
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-01-27 21:13:12 +0900

    Add complex extensions.

---
+++ NumFlat/ComplexEx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+namespace NumFlat
+{
+    public static class ComplexEx
+    {
+        public static Complex Conjugate(this Complex value)
+        {
+            return new Complex(value.Real, -value.Imaginary);
+        }
+    }
+}
---
+++ NumFlatTest/ComplexExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using NUnit.Framework;
+using NumFlat;
+
+namespace NumFlatTest
+{
+    public class ComplexExtensions
+    {
+        [TestCase(0.0, 0.0)]
+        [TestCase(1.0, 0.0)]
+        [TestCase(1.0, 2.0)]
+        [TestCase(-2.0, -3.0)]
+        [TestCase(4.0, -5.0)]
+        public void Conjugate(double real, double imaginary)
+        {
+            var value = new Complex(real, imaginary);
+            var conjugated = value.Conjugate();
+            Assert.That(conjugated.Real, Is.EqualTo(real).Within(1.0E-12));
+            Assert.That(conjugated.Imaginary, Is.EqualTo(-imaginary).Within(1.0E-12));
+        }
+    }
+}
