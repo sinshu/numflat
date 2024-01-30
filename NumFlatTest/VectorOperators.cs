0c13930f998870a27e211489117e7ea5b02efe18
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-01-30 12:19:35 +0900

    Improve tests.

--- NumFlatTest/VectorOperators.cs
+++ NumFlatTest/VectorOperators.cs
@@ -9,16 +9,16 @@ namespace NumFlatTest
 {
     public class VectorOperators
     {
-        [TestCase(1, 42, 1, 57, 1)]
-        [TestCase(1, 42, 3, 57, 2)]
-        [TestCase(3, 42, 1, 57, 1)]
-        [TestCase(3, 42, 3, 57, 2)]
-        [TestCase(5, 42, 1, 57, 2)]
-        [TestCase(11, 42, 7, 57, 2)]
-        public void Add(int count, int xSeed, int xStride, int ySeed, int yStride)
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 3, 2)]
+        [TestCase(5, 1, 2)]
+        [TestCase(11, 7, 2)]
+        public void Add(int count, int xStride, int yStride)
         {
-            var x = Utilities.CreateRandomVectorDouble(xSeed, count, xStride);
-            var y = Utilities.CreateRandomVectorDouble(ySeed, count, yStride);
+            var x = Utilities.CreateRandomVectorDouble(42, count, xStride);
+            var y = Utilities.CreateRandomVectorDouble(57, count, yStride);
             var destination = x + y;
 
             var expected = x.Zip(y, (val1, val2) => val1 + val2).ToArray();
@@ -26,16 +26,16 @@ namespace NumFlatTest
             Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
         }
 
-        [TestCase(1, 42, 1, 57, 1)]
-        [TestCase(1, 42, 3, 57, 2)]
-        [TestCase(3, 42, 1, 57, 1)]
-        [TestCase(3, 42, 3, 57, 2)]
-        [TestCase(5, 42, 1, 57, 2)]
-        [TestCase(11, 42, 7, 57, 2)]
-        public void Sub(int count, int xSeed, int xStride, int ySeed, int yStride)
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 3, 2)]
+        [TestCase(5, 1, 2)]
+        [TestCase(11, 7, 2)]
+        public void Sub(int count, int xStride, int yStride)
         {
-            var x = Utilities.CreateRandomVectorDouble(xSeed, count, xStride);
-            var y = Utilities.CreateRandomVectorDouble(ySeed, count, yStride);
+            var x = Utilities.CreateRandomVectorDouble(42, count, xStride);
+            var y = Utilities.CreateRandomVectorDouble(57, count, yStride);
             var destination = x - y;
 
             var expected = x.Zip(y, (val1, val2) => val1 - val2).ToArray();
@@ -43,15 +43,15 @@ namespace NumFlatTest
             Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
         }
 
-        [TestCase(1, 42, 1, 1.5, 1)]
-        [TestCase(1, 42, 3, 2.3, 4)]
-        [TestCase(3, 42, 1, 0.1, 1)]
-        [TestCase(3, 42, 3, 0.7, 4)]
-        [TestCase(5, 42, 1, 3.5, 3)]
-        [TestCase(11, 42, 7, 7.9, 5)]
-        public void Mul1(int count, int xSeed, int xStride, double y, int dstStride)
+        [TestCase(1, 1, 1.5, 1)]
+        [TestCase(1, 3, 2.3, 4)]
+        [TestCase(3, 1, 0.1, 1)]
+        [TestCase(3, 3, 0.7, 4)]
+        [TestCase(5, 1, 3.5, 3)]
+        [TestCase(11, 7, 7.9, 5)]
+        public void Mul1(int count, int xStride, double y, int dstStride)
         {
-            var x = Utilities.CreateRandomVectorDouble(xSeed, count, xStride);
+            var x = Utilities.CreateRandomVectorDouble(42, count, xStride);
             var destination = x * y;
 
             var expected = x.Select(value => value * y).ToArray();
@@ -59,15 +59,15 @@ namespace NumFlatTest
             Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
         }
 
-        [TestCase(1, 42, 1, 1.5, 1)]
-        [TestCase(1, 42, 3, 2.3, 4)]
-        [TestCase(3, 42, 1, 0.1, 1)]
-        [TestCase(3, 42, 3, 0.7, 4)]
-        [TestCase(5, 42, 1, 3.5, 3)]
-        [TestCase(11, 42, 7, 7.9, 5)]
-        public void Mul2(int count, int xSeed, int xStride, double y, int dstStride)
+        [TestCase(1, 1, 1.5, 1)]
+        [TestCase(1, 3, 2.3, 4)]
+        [TestCase(3, 1, 0.1, 1)]
+        [TestCase(3, 3, 0.7, 4)]
+        [TestCase(5, 1, 3.5, 3)]
+        [TestCase(11, 7, 7.9, 5)]
+        public void Mul2(int count, int xStride, double y, int dstStride)
         {
-            var x = Utilities.CreateRandomVectorDouble(xSeed, count, xStride);
+            var x = Utilities.CreateRandomVectorDouble(42, count, xStride);
             var destination = y * x;
 
             var expected = x.Select(value => value * y).ToArray();
@@ -75,48 +75,48 @@ namespace NumFlatTest
             Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
         }
 
-        [TestCase(1, 42, 1, 57, 1)]
-        [TestCase(1, 42, 3, 57, 2)]
-        [TestCase(3, 42, 1, 57, 1)]
-        [TestCase(3, 42, 3, 57, 2)]
-        [TestCase(5, 42, 1, 57, 2)]
-        [TestCase(11, 42, 7, 57, 2)]
-        public void DotProductSingle(int count, int xSeed, int xStride, int ySeed, int yStride)
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 3, 2)]
+        [TestCase(5, 1, 2)]
+        [TestCase(11, 7, 2)]
+        public void DotProductSingle(int count, int xStride, int yStride)
         {
-            var x = Utilities.CreateRandomVectorSingle(xSeed, count, xStride);
-            var y = Utilities.CreateRandomVectorSingle(ySeed, count, yStride);
+            var x = Utilities.CreateRandomVectorSingle(42, count, xStride);
+            var y = Utilities.CreateRandomVectorSingle(57, count, yStride);
             var actual = x * y;
 
             var expected = x.Zip(y, (val1, val2) => val1 * val2).Sum();
             Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
         }
 
-        [TestCase(1, 42, 1, 57, 1)]
-        [TestCase(1, 42, 3, 57, 2)]
-        [TestCase(3, 42, 1, 57, 1)]
-        [TestCase(3, 42, 3, 57, 2)]
-        [TestCase(5, 42, 1, 57, 2)]
-        [TestCase(11, 42, 7, 57, 2)]
-        public void DotProductDouble(int count, int xSeed, int xStride, int ySeed, int yStride)
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 3, 2)]
+        [TestCase(5, 1, 2)]
+        [TestCase(11, 7, 2)]
+        public void DotProductDouble(int count, int xStride, int yStride)
         {
-            var x = Utilities.CreateRandomVectorDouble(xSeed, count, xStride);
-            var y = Utilities.CreateRandomVectorDouble(ySeed, count, yStride);
+            var x = Utilities.CreateRandomVectorDouble(42, count, xStride);
+            var y = Utilities.CreateRandomVectorDouble(57, count, yStride);
             var actual = x * y;
 
             var expected = x.Zip(y, (val1, val2) => val1 * val2).Sum();
             Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
         }
 
-        [TestCase(1, 42, 1, 57, 1)]
-        [TestCase(1, 42, 3, 57, 2)]
-        [TestCase(3, 42, 1, 57, 1)]
-        [TestCase(3, 42, 3, 57, 2)]
-        [TestCase(5, 42, 1, 57, 2)]
-        [TestCase(11, 42, 7, 57, 2)]
-        public void DotProductComplex(int count, int xSeed, int xStride, int ySeed, int yStride)
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 3, 2)]
+        [TestCase(5, 1, 2)]
+        [TestCase(11, 7, 2)]
+        public void DotProductComplex(int count, int xStride, int yStride)
         {
-            var x = Utilities.CreateRandomVectorComplex(xSeed, count, xStride);
-            var y = Utilities.CreateRandomVectorComplex(ySeed, count, yStride);
+            var x = Utilities.CreateRandomVectorComplex(42, count, xStride);
+            var y = Utilities.CreateRandomVectorComplex(57, count, yStride);
             var actual = x * y;
 
             var expected = x.Zip(y, (val1, val2) => val1 * val2).Aggregate((sum, next) => sum + next);
