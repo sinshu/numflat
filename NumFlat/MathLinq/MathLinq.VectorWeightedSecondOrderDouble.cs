1df01d834e742cf4c96a9c7421cd795fe22fcc6b
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-02-10 20:18:09 +0900

    Add weighted mean and covariance for vectors.

---
+++ NumFlat/MathLinq/MathLinq.VectorWeightedSecondOrderDouble.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenBlasSharp;
+
+namespace NumFlat
+{
+    public static partial class MathLinq
+    {
+        /// <summary>
+        /// Computes the mean vector from a sequence of vectors and their weights.
+        /// </summary>
+        /// <param name="xs">
+        /// The source vectors.
+        /// </param>
+        /// <param name="weights">
+        /// The weights of the source vectors.
+        /// </param>
+        /// <param name="destination">
+        /// The destination of the mean vector.
+        /// </param>
+        public static void Mean(IEnumerable<Vec<double>> xs, IEnumerable<double> weights, in Vec<double> destination)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfNull(weights, nameof(weights));
+            ThrowHelper.ThrowIfEmpty(destination, nameof(destination));
+
+            destination.Clear();
+
+            var w1Sum = 0.0;
+            using (var exs = xs.GetEnumerator())
+            using (var eweights = weights.GetEnumerator())
+            {
+                while (true)
+                {
+                    var xsHasNext = exs.MoveNext();
+                    var weightsHasNext = eweights.MoveNext();
+                    if (xsHasNext != weightsHasNext)
+                    {
+                        throw new ArgumentException("The number of source vectors and weights must match.");
+                    }
+
+                    if (xsHasNext)
+                    {
+                        var x = exs.Current;
+                        var w = eweights.Current;
+
+                        if (x.Count != destination.Count)
+                        {
+                            throw new ArgumentException("All the source vectors must have the same length as the destination.");
+                        }
+
+                        if (w < 0)
+                        {
+                            throw new ArgumentException("Negative weight values are not allowed.");
+                        }
+
+                        AccumulateMean(x, w, destination);
+                        w1Sum += w;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (w1Sum <= 0)
+                {
+                    throw new ArgumentException("The number of source vectors is not sufficient.");
+                }
+
+                Vec.Div(destination, w1Sum, destination);
+            }
+        }
+
+        /// <summary>
+        /// Computes the mean vector from a sequence of vectors and their weights.
+        /// </summary>
+        /// <param name="xs">
+        /// The source vectors.
+        /// </param>
+        /// <param name="weights">
+        /// The weights of the source vectors.
+        /// </param>
+        /// <returns>
+        /// The mean vector.
+        /// </returns>
+        public static unsafe Vec<double> Mean(this IEnumerable<Vec<double>> xs, IEnumerable<double> weights)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfNull(weights, nameof(weights));
+
+            var destination = new Vec<double>();
+
+            var w1Sum = 0.0;
+            using (var exs = xs.GetEnumerator())
+            using (var eweights = weights.GetEnumerator())
+            {
+
+                while (true)
+                {
+                    var xsHasNext = exs.MoveNext();
+                    var weightsHasNext = eweights.MoveNext();
+                    if (xsHasNext != weightsHasNext)
+                    {
+                        throw new ArgumentException("The number of vectors and weights must match.");
+                    }
+
+                    if (xsHasNext)
+                    {
+                        var x = exs.Current;
+                        var w = eweights.Current;
+
+                        if (destination.Count == 0)
+                        {
+                            if (x.Count == 0)
+                            {
+                                new ArgumentException("Empty vectors are not allowed.");
+                            }
+
+                            destination = new Vec<double>(x.Count);
+                        }
+
+                        if (x.Count != destination.Count)
+                        {
+                            throw new ArgumentException("All the vectors must have the same length.");
+                        }
+
+                        if (w < 0)
+                        {
+                            throw new ArgumentException("Negative weight values are not allowed.");
+                        }
+
+                        AccumulateMean(x, w, destination);
+                        w1Sum += w;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            if (w1Sum <= 0)
+            {
+                throw new ArgumentException("The number of source vectors is not sufficient.");
+            }
+
+            Vec.Div(destination, w1Sum, destination);
+
+            return destination;
+        }
+
+        /// <summary>
+        /// Computes the covariance matrix from a sequence of vectors and their weights.
+        /// </summary>
+        /// <param name="xs">
+        /// The source vectors.
+        /// </param>
+        /// <param name="weights">
+        /// The weights of the source vectors.
+        /// </param>
+        /// <param name="mean">
+        /// The pre-computed mean vector of the source vectors.
+        /// </param>
+        /// <param name="destination">
+        /// The destination of the covariance matrix.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        public static unsafe void Covariance(IEnumerable<Vec<double>> xs, IEnumerable<double> weights, in Vec<double> mean, in Mat<double> destination, int ddof)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfNull(weights, nameof(weights));
+            ThrowHelper.ThrowIfEmpty(mean, nameof(mean));
+            ThrowHelper.ThrowIfEmpty(destination, nameof(destination));
+
+            if (destination.RowCount != mean.Count)
+            {
+                throw new ArgumentException("The number of rows of the destination must match the length of the mean vector.");
+            }
+
+            if (destination.ColCount != mean.Count)
+            {
+                throw new ArgumentException("The number of columns of the destination must match the length of the mean vector.");
+            }
+
+            if (ddof < 0)
+            {
+                throw new ArgumentException("The delta degrees of freedom must be a non-negative value.");
+            }
+
+            using var ucentered = new TemporalVector<double>(mean.Count);
+            ref readonly var centered = ref ucentered.Item;
+
+            destination.Clear();
+
+            var w1Sum = 0.0;
+            var w2Sum = 0.0;
+            using (var exs = xs.GetEnumerator())
+            using (var eweights = weights.GetEnumerator())
+            {
+                fixed (double* pd = destination.Memory.Span)
+                fixed (double* pc = centered.Memory.Span)
+                {
+                    while (true)
+                    {
+                        var xsHasNext = exs.MoveNext();
+                        var weightsHasNext = eweights.MoveNext();
+                        if (xsHasNext != weightsHasNext)
+                        {
+                            throw new ArgumentException("The number of source vectors and weights must match.");
+                        }
+
+                        if (xsHasNext)
+                        {
+                            var x = exs.Current;
+                            var w = eweights.Current;
+
+                            if (x.Count != mean.Count)
+                            {
+                                throw new ArgumentException("All the source vectors must have the same length as the mean vector.");
+                            }
+
+                            if (w < 0)
+                            {
+                                throw new ArgumentException("Negative weight values are not allowed.");
+                            }
+
+                            Vec.Sub(x, mean, centered);
+                            Blas.Dsyr(
+                                Order.ColMajor,
+                                Uplo.Lower,
+                                destination.RowCount,
+                                w,
+                                pc, centered.Stride,
+                                pd, destination.Stride);
+                            w1Sum += w;
+                            w2Sum += w * w;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                for (var col = 1; col < destination.ColCount; col++)
+                {
+                    for (var row = 0; row < col; row++)
+                    {
+                        destination[row, col] = destination[col, row];
+                    }
+                }
+
+                var den = w1Sum - ddof * (w2Sum / w1Sum);
+                if (den <= 0)
+                {
+                    throw new ArgumentException("The number of source vectors is not sufficient.");
+                }
+
+                Mat.Div(destination, den, destination);
+            }
+        }
+
+        private static void AccumulateMean(in Vec<double> x, double w, in Vec<double> destination)
+        {
+            var sx = x.Memory.Span;
+            var sd = destination.Memory.Span;
+            var px = 0;
+            var pd = 0;
+            while (pd < sd.Length)
+            {
+                sd[pd] += w * sx[px];
+                px += x.Stride;
+                pd += destination.Stride;
+            }
+        }
+    }
+}
---
+++ NumFlatTest/MathLinqTest/MathLinqTest.VectorWeightedSecondOrderDouble.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics.Statistics;
+using NUnit.Framework;
+using NumFlat;
+
+using MVec = MathNet.Numerics.LinearAlgebra.Vector<double>;
+using MMat = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+
+namespace NumFlatTest
+{
+    public class MathLinqTest_VectorWeightedSecondOrderDouble
+    {
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, 3)]
+        [TestCase(1, 3, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 1, 4)]
+        [TestCase(3, 10, 1)]
+        [TestCase(3, 10, 2)]
+        [TestCase(5, 20, 3)]
+        public void Mean(int dim, int count, int dstStride)
+        {
+            var data = CreateData(42, dim, count);
+            var weights = CreateWeights(57, count);
+            var expected = MathNetMean(data, weights);
+
+            var actual = TestVector.RandomDouble(0, dim, dstStride);
+            MathLinq.Mean(data.Select(x => x.ToVector()), weights, actual);
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+
+            TestVector.FailIfOutOfRangeWrite(actual);
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, 3)]
+        [TestCase(1, 3, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 1, 4)]
+        [TestCase(3, 10, 1)]
+        [TestCase(3, 10, 2)]
+        [TestCase(5, 20, 3)]
+        public void Mean_SameWeights(int dim, int count, int dstStride)
+        {
+            var data = CreateData(42, dim, count);
+            var weights = Enumerable.Repeat(100.0, count);
+            var expected = data.Select(x => x.ToVector()).Mean();
+
+            var actual = TestVector.RandomDouble(0, dim, dstStride);
+            MathLinq.Mean(data.Select(x => x.ToVector()), weights, actual);
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+
+            TestVector.FailIfOutOfRangeWrite(actual);
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(1, 3)]
+        [TestCase(3, 1)]
+        [TestCase(3, 10)]
+        [TestCase(5, 20)]
+        public void Mean_ExtensionMethod(int dim, int count)
+        {
+            var data = CreateData(42, dim, count);
+            var weights = CreateWeights(57, count);
+            var expected = MathNetMean(data, weights);
+            var actual = data.Select(x => x.ToVector()).Mean(weights);
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(1, 3)]
+        [TestCase(3, 1)]
+        [TestCase(3, 10)]
+        [TestCase(5, 20)]
+        public void Mean_ExtensionMethod_SameWeights(int dim, int count)
+        {
+            var data = CreateData(42, dim, count);
+            var weights = Enumerable.Repeat(100.0, count);
+            var expected = data.Select(x => x.ToVector()).Mean();
+            var actual = data.Select(x => x.ToVector()).Mean(weights);
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+        }
+
+        [TestCase(1, 1, 0, 1, 1)]
+        [TestCase(1, 1, 0, 3, 3)]
+        [TestCase(1, 3, 0, 1, 1)]
+        [TestCase(1, 3, 0, 3, 3)]
+        [TestCase(1, 3, 1, 1, 1)]
+        [TestCase(1, 3, 1, 3, 3)]
+        [TestCase(3, 1, 0, 1, 3)]
+        [TestCase(3, 1, 0, 2, 4)]
+        [TestCase(3, 10, 1, 1, 3)]
+        [TestCase(3, 10, 1, 4, 7)]
+        [TestCase(5, 20, 2, 1, 5)]
+        [TestCase(5, 20, 2, 5, 6)]
+        public void Covariance(int dim, int count, int ddof, int meanStride, int covStride)
+        {
+            var data = CreateData(42, dim, count);
+            var weights = Enumerable.Repeat(100.0, count);
+            var expected = MathNetCov(data, weights, ddof);
+
+            var mean = TestVector.RandomDouble(0, dim, meanStride);
+            MathLinq.Mean(data.Select(x => x.ToVector()), weights, mean);
+
+            var actual = TestMatrix.RandomDouble(0, dim, dim, covStride);
+            using (mean.EnsureUnchanged())
+            {
+                MathLinq.Covariance(data.Select(x => x.ToVector()), weights, mean, actual, ddof);
+            }
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+
+            TestVector.FailIfOutOfRangeWrite(mean);
+            TestMatrix.FailIfOutOfRangeWrite(actual);
+        }
+
+        [TestCase(1, 1, 0, 1, 1)]
+        [TestCase(1, 1, 0, 3, 3)]
+        [TestCase(1, 3, 0, 1, 1)]
+        [TestCase(1, 3, 0, 3, 3)]
+        [TestCase(1, 3, 1, 1, 1)]
+        [TestCase(1, 3, 1, 3, 3)]
+        [TestCase(3, 1, 0, 1, 3)]
+        [TestCase(3, 1, 0, 2, 4)]
+        [TestCase(3, 10, 1, 1, 3)]
+        [TestCase(3, 10, 1, 4, 7)]
+        [TestCase(5, 20, 2, 1, 5)]
+        [TestCase(5, 20, 2, 5, 6)]
+        public void Covariance_SameWeights(int dim, int count, int ddof, int meanStride, int covStride)
+        {
+            var data = CreateData(42, dim, count);
+            var weights = Enumerable.Repeat(100.0, count);
+            var expected = data.Select(x => x.ToVector()).Covariance(ddof);
+
+            var mean = TestVector.RandomDouble(0, dim, meanStride);
+            MathLinq.Mean(data.Select(x => x.ToVector()), weights, mean);
+
+            var actual = TestMatrix.RandomDouble(0, dim, dim, covStride);
+            using (mean.EnsureUnchanged())
+            {
+                MathLinq.Covariance(data.Select(x => x.ToVector()), weights, mean, actual, ddof);
+            }
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+
+            TestVector.FailIfOutOfRangeWrite(mean);
+            TestMatrix.FailIfOutOfRangeWrite(actual);
+        }
+
+        private static MVec MathNetMean(IEnumerable<MVec> xs, IEnumerable<double> weights)
+        {
+            MVec sum = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(xs.First().Count);
+            var count = 0.0;
+            foreach (var (x, w) in xs.Zip(weights))
+            {
+                sum += w * x;
+                count += w;
+            }
+            return sum / count;
+        }
+
+        private static MMat MathNetCov(IEnumerable<MVec> xs, IEnumerable<double> weights, int ddof)
+        {
+            var mean = MathNetMean(xs, weights);
+
+            MMat sum = new MathNet.Numerics.LinearAlgebra.Double.DenseMatrix(xs.First().Count);
+            var w1Sum = 0.0;
+            var w2Sum = 0.0;
+            foreach (var (x, w) in xs.Zip(weights))
+            {
+                var d = x - mean;
+                sum += w * d.OuterProduct(d);
+                w1Sum += w;
+                w2Sum += w * w;
+            }
+            var den = w1Sum - ddof * (w2Sum / w1Sum);
+            return sum / den;
+        }
+
+        private static MVec[] CreateData(int seed, int dim, int count)
+        {
+            var random = new Random(seed);
+
+            var data = new List<MVec>();
+            for (var i = 0; i < count; i++)
+            {
+                var elements = Enumerable.Range(0, dim).Select(i => random.NextDouble()).ToArray();
+                var x = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(elements);
+                data.Add(x);
+            }
+            return data.ToArray();
+        }
+
+        private static double[] CreateWeights(int seed, int count)
+        {
+            var random = new Random(seed);
+
+            var data = new List<double>();
+            for (var i = 0; i < count; i++)
+            {
+                data.Add(1 + random.NextDouble());
+            }
+            return data.ToArray();
+        }
+    }
+}
