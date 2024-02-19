75da7be9b74031f566c74b8c0757ccf2aae8fa59
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-02-19 15:54:53 +0900

    Add vector distance.

--- NumFlatTest/VecTests/VecTests.MathNorm.cs
+++ NumFlatTest/VecTests/VecTests.MathNorm.cs
@@ -18,7 +18,7 @@ namespace NumFlatTest
         public void NormSingle(int count, int stride)
         {
             var x = TestVector.RandomSingle(42, count, stride);
-            
+
             var mx = Interop.ToMathNet(x);
             var expected = mx.L2Norm();
 
@@ -415,6 +415,84 @@ namespace NumFlatTest
             NumAssert.AreSame(expected, actual, 1.0E-12);
 
             TestVector.FailIfOutOfRangeWrite(actual);
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 1, 1)]
+        [TestCase(2, 2, 3)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 2, 3)]
+        [TestCase(5, 2, 3)]
+        [TestCase(7, 3, 4)]
+        public void DistanceSingle(int count, int xStride, int yStride)
+        {
+            var x = TestVector.RandomSingle(42, count, xStride);
+            var y = TestVector.RandomSingle(57, count, yStride);
+
+            var expected1 = (x - y).Norm();
+            var expected2 = expected1 * expected1;
+
+            using (x.EnsureUnchanged())
+            using (y.EnsureUnchanged())
+            {
+                var actual1 = x.Distance(y);
+                var actual2 = x.DistanceSquared(y);
+                Assert.That(actual1, Is.EqualTo(expected1).Within(1.0E-6F));
+                Assert.That(actual2, Is.EqualTo(expected2).Within(1.0E-6F));
+            }
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 1, 1)]
+        [TestCase(2, 2, 3)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 2, 3)]
+        [TestCase(5, 2, 3)]
+        [TestCase(7, 3, 4)]
+        public void DistanceDouble(int count, int xStride, int yStride)
+        {
+            var x = TestVector.RandomDouble(42, count, xStride);
+            var y = TestVector.RandomDouble(57, count, yStride);
+
+            var expected1 = (x - y).Norm();
+            var expected2 = expected1 * expected1;
+
+            using (x.EnsureUnchanged())
+            using (y.EnsureUnchanged())
+            {
+                var actual1 = x.Distance(y);
+                var actual2 = x.DistanceSquared(y);
+                Assert.That(actual1, Is.EqualTo(expected1).Within(1.0E-12));
+                Assert.That(actual2, Is.EqualTo(expected2).Within(1.0E-12));
+            }
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 1, 1)]
+        [TestCase(2, 2, 3)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 2, 3)]
+        [TestCase(5, 2, 3)]
+        [TestCase(7, 3, 4)]
+        public void DistanceComplex(int count, int xStride, int yStride)
+        {
+            var x = TestVector.RandomComplex(42, count, xStride);
+            var y = TestVector.RandomComplex(57, count, yStride);
+
+            var expected1 = (x - y).Norm();
+            var expected2 = expected1 * expected1;
+
+            using (x.EnsureUnchanged())
+            using (y.EnsureUnchanged())
+            {
+                var actual1 = x.Distance(y);
+                var actual2 = x.DistanceSquared(y);
+                Assert.That(actual1, Is.EqualTo(expected1).Within(1.0E-12));
+                Assert.That(actual2, Is.EqualTo(expected2).Within(1.0E-12));
+            }
         }
     }
 }
