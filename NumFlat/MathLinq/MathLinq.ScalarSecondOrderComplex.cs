c1312c1400246a83704dc9f19f336a3232f1dbf7
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-02-25 21:20:45 +0900

    Add more MathLinq methods.

---
+++ NumFlat/MathLinq/MathLinq.ScalarSecondOrderComplex.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace NumFlat
+{
+    public static partial class MathLinq
+    {
+        /// <summary>
+        /// Computes the average of a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <returns>
+        /// The average of the sequence of values.
+        /// </returns>
+        public static Complex Average(this IEnumerable<Complex> xs)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+
+            var sum = Complex.Zero;
+            var count = 0;
+            foreach (var x in xs)
+            {
+                sum += x;
+                count++;
+            }
+
+            if (count == 0)
+            {
+                throw new ArgumentException("The sequence must contain at least one value.");
+            }
+
+            return sum / count;
+        }
+
+        /// <summary>
+        /// Computes the mean and variance from a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        /// <returns>
+        /// The mean and variance.
+        /// </returns>
+        public static (Complex Mean, double Variance) MeanAndVariance(this IEnumerable<Complex> xs, int ddof = 1)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+
+            if (ddof < 0)
+            {
+                throw new ArgumentException("The delta degrees of freedom must be a non-negative value.");
+            }
+
+            var mean = xs.Average();
+
+            var sum = 0.0;
+            var count = 0;
+            foreach (var x in xs)
+            {
+                var d = x - mean;
+                sum += d.MagnitudeSquared();
+                count++;
+            }
+
+            if (count - ddof <= 0)
+            {
+                throw new ArgumentException("The number of source values is not sufficient.");
+            }
+
+            var variance = sum / (count - ddof);
+
+            return (mean, variance);
+        }
+
+        /// <summary>
+        /// Computes the mean and variance from a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        /// <returns>
+        /// The mean and variance.
+        /// </returns>
+        public static (Complex Mean, double StandardDeviation) MeanAndStandardDeviation(this IEnumerable<Complex> xs, int ddof = 1)
+        {
+            var (mean, variance) = xs.MeanAndVariance(ddof);
+            return (mean, Math.Sqrt(variance));
+        }
+
+        /// <summary>
+        /// Computes the variance from a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        /// <returns>
+        /// The variance of the source values.
+        /// </returns>
+        public static double Variance(this IEnumerable<Complex> xs, int ddof = 1)
+        {
+            return xs.MeanAndVariance(ddof).Variance;
+        }
+
+        /// <summary>
+        /// Computes the standard deviation from a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        /// <returns>
+        /// The standard deviation of the source values.
+        /// </returns>
+        public static double StandardDeviation(this IEnumerable<Complex> xs, int ddof = 1)
+        {
+            return Math.Sqrt(xs.MeanAndVariance(ddof).Variance);
+        }
+    }
+}
---
+++ NumFlatTest/MathLinqTests/MathLinqTests.ScalarSecondOrderComplex.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using NUnit.Framework;
+using NumFlat;
+
+namespace NumFlatTest
+{
+    public class MathLinqTests_ScalarSecondOrderComplex
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void Average(int count)
+        {
+            var random = new Random(42);
+            var values = Enumerable.Range(0, count).Select(i => new Complex(random.NextDouble(), random.NextDouble())).ToArray();
+
+            var actual = values.Average();
+            var expected = new Complex(values.Select(x => x.Real).Average(), values.Select(x => x.Imaginary).Average());
+
+            Assert.That(actual.Real, Is.EqualTo(expected.Real).Within(1.0E-12));
+            Assert.That(actual.Imaginary, Is.EqualTo(expected.Imaginary).Within(1.0E-12));
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(5, 0)]
+        [TestCase(10, 1)]
+        public void Variance(int count, int ddof)
+        {
+            var random = new Random(42);
+            var values = Enumerable.Range(0, count).Select(i => new Complex(random.NextDouble(), random.NextDouble())).ToArray();
+
+            var actual = values.Variance(ddof);
+            var expected = MathNetVariance(values, ddof);
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(5, 0)]
+        [TestCase(10, 1)]
+        public void MeanAndVariance(int count, int ddof)
+        {
+            var random = new Random(42);
+            var values = Enumerable.Range(0, count).Select(i => new Complex(random.NextDouble(), random.NextDouble())).ToArray();
+
+            var actual = values.MeanAndVariance(ddof);
+
+            var expectedMean = values.Average();
+            var expectedVariance = MathNetVariance(values, ddof);
+
+            Assert.That(actual.Mean, Is.EqualTo(expectedMean).Within(1.0E-12));
+            Assert.That(actual.Variance, Is.EqualTo(expectedVariance).Within(1.0E-12));
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(5, 0)]
+        [TestCase(10, 1)]
+        public void StandardDeviation(int count, int ddof)
+        {
+            var random = new Random(42);
+            var values = Enumerable.Range(0, count).Select(i => new Complex(random.NextDouble(), random.NextDouble())).ToArray();
+
+            var actual = values.StandardDeviation(ddof);
+            var expected = MathNetStandardDeviation(values, ddof);
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(5, 0)]
+        [TestCase(10, 1)]
+        public void MeanAndStandardDeviation(int count, int ddof)
+        {
+            var random = new Random(42);
+            var values = Enumerable.Range(0, count).Select(i => new Complex(random.NextDouble(), random.NextDouble())).ToArray();
+
+            var actual = values.MeanAndStandardDeviation(ddof);
+
+            var expectedMean = values.Average();
+            var expectedStandardDeviation = MathNetStandardDeviation(values, ddof);
+
+            Assert.That(actual.Mean.Real, Is.EqualTo(expectedMean.Real).Within(1.0E-12));
+            Assert.That(actual.Mean.Imaginary, Is.EqualTo(expectedMean.Imaginary).Within(1.0E-12));
+            Assert.That(actual.StandardDeviation, Is.EqualTo(expectedStandardDeviation).Within(1.0E-12));
+        }
+
+        private static double MathNetVariance(IEnumerable<Complex> xs, int ddof)
+        {
+            var mean = xs.Average();
+            var sum = 0.0;
+            var count = 0;
+            foreach (var x in xs)
+            {
+                var d = x - mean;
+                sum += (d * d.Conjugate()).Real;
+                count++;
+            }
+            return sum / (count - ddof);
+        }
+
+        private static double MathNetStandardDeviation(IEnumerable<Complex> xs, int ddof)
+        {
+            return Math.Sqrt(MathNetVariance(xs, ddof));
+        }
+    }
+}
