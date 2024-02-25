9c3268f4521a44d4f293816dad832d0637988358
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-02-25 19:56:10 +0900

    Add more methods.

--- NumFlat/MathLinq/MathLinq.ScalarSecondOrderDouble.cs
+++ NumFlat/MathLinq/MathLinq.ScalarSecondOrderDouble.cs
@@ -7,47 +7,6 @@ namespace NumFlat
     public static partial class MathLinq
     {
         /// <summary>
-        /// Computes the variance from a sequence of values.
-        /// </summary>
-        /// <param name="xs">
-        /// The source values.
-        /// </param>
-        /// <param name="mean">
-        /// The pre-computed mean of the source values.
-        /// </param>
-        /// <param name="ddof">
-        /// The delta degrees of freedom.
-        /// </param>
-        /// <returns>
-        /// The variance of the source values.
-        /// </returns>
-        public static double Variance(this IEnumerable<double> xs, double mean, int ddof = 1)
-        {
-            ThrowHelper.ThrowIfNull(xs, nameof(xs));
-
-            if (ddof < 0)
-            {
-                throw new ArgumentException("The delta degrees of freedom must be a non-negative value.");
-            }
-
-            var sum = 0.0;
-            var count = 0;
-            foreach (var x in xs)
-            {
-                var d = x - mean;
-                sum += d * d;
-                count++;
-            }
-
-            if (count - ddof <= 0)
-            {
-                throw new ArgumentException("The number of source values is not sufficient.");
-            }
-
-            return sum / (count - ddof);
-        }
-
-        /// <summary>
         /// Computes the mean and variance from a sequence of values.
         /// </summary>
         /// <param name="xs">
@@ -69,8 +28,76 @@ namespace NumFlat
             }
 
             var mean = xs.Average();
-            var variance = xs.Variance(mean, ddof);
+
+            var sum = 0.0;
+            var count = 0;
+            foreach (var x in xs)
+            {
+                var d = x - mean;
+                sum += d * d;
+                count++;
+            }
+
+            if (count - ddof <= 0)
+            {
+                throw new ArgumentException("The number of source values is not sufficient.");
+            }
+
+            var variance = sum / (count - ddof);
+
             return (mean, variance);
+        }
+
+        /// <summary>
+        /// Computes the mean and variance from a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        /// <returns>
+        /// The mean and variance.
+        /// </returns>
+        public static (double Mean, double StandardDeviation) MeanAndStandardDeviation(this IEnumerable<double> xs, int ddof = 1)
+        {
+            var (mean, variance) = xs.MeanAndVariance(ddof);
+            return (mean, Math.Sqrt(variance));
+        }
+
+        /// <summary>
+        /// Computes the variance from a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        /// <returns>
+        /// The variance of the source values.
+        /// </returns>
+        public static double Variance(this IEnumerable<double> xs, int ddof = 1)
+        {
+            return xs.MeanAndVariance(ddof).Variance;
+        }
+
+        /// <summary>
+        /// Computes the standard deviation from a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="ddof">
+        /// The delta degrees of freedom.
+        /// </param>
+        /// <returns>
+        /// The standard deviation of the source values.
+        /// </returns>
+        public static double StandardDeviation(this IEnumerable<double> xs, int ddof = 1)
+        {
+            return Math.Sqrt(xs.MeanAndVariance(ddof).Variance);
         }
     }
 }
--- NumFlatTest/MathLinqTests/MathLinqTests.ScalarSecondOrderDouble.cs
+++ NumFlatTest/MathLinqTests/MathLinqTests.ScalarSecondOrderDouble.cs
@@ -19,7 +19,7 @@ namespace NumFlatTest
             var random = new Random(42);
             var values = Enumerable.Range(0, count).Select(i => random.NextDouble()).ToArray();
 
-            var actual = values.Variance(values.Average(), ddof);
+            var actual = values.Variance(ddof);
             var expected = MathNetVariance(values, ddof);
             Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
         }
@@ -43,6 +43,40 @@ namespace NumFlatTest
             Assert.That(actual.Variance, Is.EqualTo(expectedVariance).Within(1.0E-12));
         }
 
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(5, 0)]
+        [TestCase(10, 1)]
+        public void StandardDeviation(int count, int ddof)
+        {
+            var random = new Random(42);
+            var values = Enumerable.Range(0, count).Select(i => random.NextDouble()).ToArray();
+
+            var actual = values.StandardDeviation(ddof);
+            var expected = MathNetStandardDeviation(values, ddof);
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(5, 0)]
+        [TestCase(10, 1)]
+        public void MeanAndStandardDeviation(int count, int ddof)
+        {
+            var random = new Random(42);
+            var values = Enumerable.Range(0, count).Select(i => random.NextDouble()).ToArray();
+
+            var actual = values.MeanAndStandardDeviation(ddof);
+
+            var expectedMean = values.Average();
+            var expectedStandardDeviation = MathNetStandardDeviation(values, ddof);
+
+            Assert.That(actual.Mean, Is.EqualTo(expectedMean).Within(1.0E-12));
+            Assert.That(actual.StandardDeviation, Is.EqualTo(expectedStandardDeviation).Within(1.0E-12));
+        }
+
         private static double MathNetVariance(IEnumerable<double> xs, int ddof)
         {
             if (ddof == 0)
@@ -52,6 +86,22 @@ namespace NumFlatTest
             else if (ddof == 1)
             {
                 return MathNet.Numerics.Statistics.Statistics.Variance(xs);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        private static double MathNetStandardDeviation(IEnumerable<double> xs, int ddof)
+        {
+            if (ddof == 0)
+            {
+                return MathNet.Numerics.Statistics.Statistics.PopulationStandardDeviation(xs);
+            }
+            else if (ddof == 1)
+            {
+                return MathNet.Numerics.Statistics.Statistics.StandardDeviation(xs);
             }
             else
             {
