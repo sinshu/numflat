d11f8317e7012f33ffd641cbfe6853ddaf4555ae
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-02-26 22:08:38 +0900

    Add stubs for skewness and kurtosis.

---
+++ NumFlat/MathLinq/MathLinq.HigherOrder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumFlat
+{
+    public static partial class MathLinq
+    {
+        /// <summary>
+        /// Computes the skewness of a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="unbiased">
+        /// True for an unbiased estimator, false for a biased estimator.
+        /// </param>
+        /// <returns>
+        /// The skewness of the sequence of values.
+        /// </returns>
+        public static double Skewness(this IEnumerable<double> xs, bool unbiased = true)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Computes the kurtosis of a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="unbiased">
+        /// True for an unbiased estimator, false for a biased estimator.
+        /// </param>
+        /// <returns>
+        /// The kurtosis of the sequence of values.
+        /// </returns>
+        public static double Kurtosis(this IEnumerable<double> xs, bool unbiased = true)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+
+            throw new NotImplementedException();
+        }
+    }
+}
