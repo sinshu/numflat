629feb8ac33baf86b0bda8854645120b2ca4538e
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-02-26 12:51:27 +0900

    Add weighted average to IEnumerable<double>.

---
+++ NumFlat/MathLinq/MathLinq.ScalarWeightedSecondOrderDouble.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumFlat
+{
+    public static partial class MathLinq
+    {
+        /// <summary>
+        /// Computes the weighted average of a sequence of values.
+        /// </summary>
+        /// <param name="xs">
+        /// The source values.
+        /// </param>
+        /// <param name="weights">
+        /// The weights of the source values.
+        /// </param>
+        /// <returns>
+        /// The weighted average of the sequence of values.
+        /// </returns>
+        public static double Average(this IEnumerable<double> xs, IEnumerable<double> weights)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfNull(weights, nameof(weights));
+
+            var xSum = 0.0;
+            var w1Sum = 0.0;
+            using (var exs = xs.GetEnumerator())
+            using (var eweights = weights.GetEnumerator())
+            {
+                while (true)
+                {
+                    var xsHasNext = exs.MoveNext();
+                    var weightsHasNext = eweights.MoveNext();
+                    if (xsHasNext != weightsHasNext)
+                    {
+                        throw new ArgumentException("The number of source values and weights must match.");
+                    }
+
+                    if (xsHasNext)
+                    {
+                        var x = exs.Current;
+                        var w = eweights.Current;
+
+                        if (w < 0)
+                        {
+                            throw new ArgumentException("Negative weight values are not allowed.");
+                        }
+
+                        xSum += w * x;
+                        w1Sum += w;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (w1Sum <= 0)
+                {
+                    throw new ArgumentException("The number of source vectors is not sufficient.");
+                }
+
+                return xSum / w1Sum;
+            }
+        }
+    }
+}
---
+++ NumFlatTest/MathLinqTests/MathLinqTests.ScalarWeightedSecondOrderDouble.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using NUnit.Framework;
+using NumFlat;
+
+namespace NumFlatTest
+{
+    public class MathLinqTests_ScalarWeightedSecondOrderDouble
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void Average(int count)
+        {
+            var random = new Random(42);
+            var xs = Enumerable.Range(0, count).Select(i => random.NextDouble()).ToArray();
+            var weights = Enumerable.Range(0, count).Select(i => random.NextDouble()).ToArray();
+
+            var actual = xs.Average(weights);
+            var expected = RefAverage(xs, weights);
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+        }
+
+        private static double RefAverage(IEnumerable<double> xs, IEnumerable<double> weights)
+        {
+            var xSum = 0.0;
+            var wSUm = 0.0;
+            foreach (var (weight, x) in weights.Zip(xs))
+            {
+                xSum += weight * x;
+                wSUm += weight;
+            }
+            return xSum / wSUm;
+        }
+    }
+}
