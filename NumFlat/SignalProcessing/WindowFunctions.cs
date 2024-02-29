f436ebe1e460a703697389192ed26c74474f83fa
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-02-29 17:50:56 +0900

    Add window functions.

---
+++ NumFlat/SignalProcessing/WindowFunctions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace NumFlat.SignalProcessing
+{
+    /// <summary>
+    /// Provides window functions.
+    /// </summary>
+    public static class WindowFunctions
+    {
+        /// <summary>
+        /// Creates a Hann window.
+        /// </summary>
+        /// <param name="length">
+        /// The length of the window.
+        /// </param>
+        /// <returns>
+        /// The window with the specified length.
+        /// </returns>
+        public static Vec<double> Hann(int length)
+        {
+            if (length <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "The length must be a positive value.");
+            }
+
+            var window = new double[length];
+            for (var i = 0; i < window.Length; i++)
+            {
+                var theta = 2 * Math.PI * i / length;
+                window[i] = 0.5 - 0.5 * Math.Cos(theta);
+            }
+            return new Vec<double>(window);
+        }
+
+        /// <summary>
+        /// Creates a squared Hann window.
+        /// </summary>
+        /// <param name="length">
+        /// The length of the window.
+        /// </param>
+        /// <returns>
+        /// The window with the specified length.
+        /// </returns>
+        public static Vec<double> HannSquared(int length)
+        {
+            if (length <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "The length must be a positive value.");
+            }
+
+            var window = new double[length];
+            for (var i = 0; i < window.Length; i++)
+            {
+                var theta = 2 * Math.PI * i / length;
+                window[i] = Math.Sqrt(0.5 - 0.5 * Math.Cos(theta));
+            }
+            return new Vec<double>(window);
+        }
+
+        /// <summary>
+        /// Creates a Hamming window.
+        /// </summary>
+        /// <param name="length">
+        /// The length of the window.
+        /// </param>
+        /// <returns>
+        /// The window with the specified length.
+        /// </returns>
+        public static Vec<double> Hamming(int length)
+        {
+            if (length <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "The length must be a positive value.");
+            }
+
+            var window = new double[length];
+            for (var i = 0; i < window.Length; i++)
+            {
+                var theta = 2 * Math.PI * i / length;
+                window[i] = 0.54 - 0.46 * Math.Cos(theta);
+            }
+            return new Vec<double>(window);
+        }
+    }
+}
---
+++ NumFlatTest/SignalProcessingTests/WindowFunctionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using NUnit.Framework;
+using MathNet.Numerics.IntegralTransforms;
+using NumFlat;
+using NumFlat.SignalProcessing;
+
+namespace NumFlatTest.SignalProcessingTests
+{
+    public class WindowFunctionTests
+    {
+        [TestCase(64)]
+        [TestCase(100)]
+        public void Hann(int length)
+        {
+            var window = WindowFunctions.Hann(length);
+
+            Assert.That(window.First(), Is.EqualTo(0.0).Within(1.0E-12));
+            Assert.That(window[length / 2], Is.EqualTo(1.0).Within(1.0E-12));
+            Assert.That(window.Last(), Is.EqualTo(0.0).Within(0.05));
+        }
+
+        [TestCase(64)]
+        [TestCase(100)]
+        public void HannSquared(int length)
+        {
+            var window = WindowFunctions.HannSquared(length);
+
+            Assert.That(window.First(), Is.EqualTo(0.0).Within(1.0E-12));
+            Assert.That(window[length / 2], Is.EqualTo(1.0).Within(1.0E-12));
+            Assert.That(window.Last(), Is.EqualTo(0.0).Within(0.05));
+        }
+
+        [TestCase(64)]
+        [TestCase(100)]
+        public void Hamming(int length)
+        {
+            var window = WindowFunctions.Hamming(length);
+
+            Assert.That(window.First(), Is.EqualTo(0.0).Within(0.1));
+            Assert.That(window[length / 2], Is.EqualTo(1.0).Within(0.1));
+            Assert.That(window.Last(), Is.EqualTo(0.0).Within(0.1));
+        }
+    }
+}
