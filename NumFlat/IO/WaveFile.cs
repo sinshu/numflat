8c3b790063c8ae76d62bab0f9a479cfcaf48647b
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-03 15:46:35 +0900

    Add ReadMono().

--- NumFlat/IO/WaveFile.cs
+++ NumFlat/IO/WaveFile.cs
@@ -39,20 +39,24 @@ namespace NumFlat.IO
                 }
 
                 SampleFormat sampleFormat = 0;
-                int channels = 0;
+                int channelCount = 0;
                 int sampleRate = 0;
                 Vec<double>[]? data = null;
                 while (true)
                 {
                     var id = ReadFourCC(reader);
                     var size = reader.ReadUInt32();
+                    if (size % 2 != 0)
+                    {
+                        size++;
+                    }
                     switch (id)
                     {
                         case "fmt ":
-                            (sampleFormat, channels, sampleRate) = ReadFormat(reader, (int)size);
+                            (sampleFormat, channelCount, sampleRate) = ReadFormat(reader, (int)size);
                             break;
                         case "data":
-                            data = ReadData(reader, (int)size, sampleFormat, channels);
+                            data = ReadData(reader, (int)size, sampleFormat, channelCount);
                             goto End;
                         default:
                             reader.BaseStream.Position += size;
@@ -70,18 +74,67 @@ namespace NumFlat.IO
         }
 
         /// <summary>
-        /// Reads the specified monaural wave file.
+        /// Reads a monaural waveform from the specified wave file.
         /// </summary>
         /// <param name="path">
         /// The path of the wave file.
         /// </param>
+        /// <param name="channel">
+        /// The channel to be read.
+        /// </param>
         /// <returns>
         /// The wave data and sample rate.
         /// </returns>
-        public static (Vec<double> Data, int SampleRate) ReadMono(string path)
+        public static (Vec<double> Data, int SampleRate) ReadMono(string path, int channel = 0)
         {
-            var (data, sampleRate) = Read(path);
-            return (data[0], sampleRate);
+            ThrowHelper.ThrowIfNull(path, nameof(path));
+
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (var reader = new BinaryReader(fs))
+            {
+                if (ReadFourCC(reader) != "RIFF")
+                {
+                    throw new InvalidDataException("The RIFF chunk was not found.");
+                }
+                reader.ReadInt32();
+                if (ReadFourCC(reader) != "WAVE")
+                {
+                    throw new InvalidDataException("The file is not a wave file.");
+                }
+
+                SampleFormat sampleFormat = 0;
+                int channelCount = 0;
+                int sampleRate = 0;
+                Vec<double> data = default;
+                while (true)
+                {
+                    var id = ReadFourCC(reader);
+                    var size = reader.ReadUInt32();
+                    if (size % 2 != 0)
+                    {
+                        size++;
+                    }
+                    switch (id)
+                    {
+                        case "fmt ":
+                            (sampleFormat, channelCount, sampleRate) = ReadFormat(reader, (int)size);
+                            break;
+                        case "data":
+                            data = ReadDataSingleChannel(reader, (int)size, sampleFormat, channelCount, channel);
+                            goto End;
+                        default:
+                            reader.BaseStream.Position += size;
+                            break;
+                    }
+                }
+
+            End:
+                if (data.Count == 0)
+                {
+                    throw new InvalidDataException("The data chunk was not found.");
+                }
+                return (data, sampleRate);
+            }
         }
 
         private static string ReadFourCC(BinaryReader reader)
@@ -98,7 +151,7 @@ namespace NumFlat.IO
             return Encoding.ASCII.GetString(data, 0, data.Length);
         }
 
-        private static (SampleFormat SampleFormat, int Channels, int SampleRate) ReadFormat(BinaryReader reader, int size)
+        private static (SampleFormat SampleFormat, int ChannelCount, int SampleRate) ReadFormat(BinaryReader reader, int size)
         {
             using var ubuffer = MemoryPool<byte>.Shared.Rent(size);
             var buffer = ubuffer.Memory.Span.Slice(0, size);
@@ -110,10 +163,10 @@ namespace NumFlat.IO
                 throw new InvalidDataException($"Unsupported sample format ({(int)sampleFormat}).");
             }
 
-            var channels = (int)BitConverter.ToInt16(buffer.Slice(2, 2));
-            if (channels <= 0)
+            var channelCount = (int)BitConverter.ToInt16(buffer.Slice(2, 2));
+            if (channelCount <= 0)
             {
-                throw new InvalidDataException($"Invalid channel count ({channels}).");
+                throw new InvalidDataException($"Invalid channel count ({channelCount}).");
             }
 
             var sampleRate = BitConverter.ToInt32(buffer.Slice(4, 4));
@@ -123,16 +176,16 @@ namespace NumFlat.IO
             }
 
             var blockAlign = (int)BitConverter.ToInt16(buffer.Slice(12, 2));
-            var expected = GetSampleSize(sampleFormat) * channels;
+            var expected = GetSampleSize(sampleFormat) * channelCount;
             if (blockAlign != expected)
             {
                 throw new InvalidDataException($"Block align is expected to be '{expected}', but was '{blockAlign}'.");
             }
 
-            return (sampleFormat, channels, sampleRate);
+            return (sampleFormat, channelCount, sampleRate);
         }
 
-        private static Vec<double>[] ReadData(BinaryReader reader, int size, SampleFormat sampleFormat, int channels)
+        private static Vec<double>[] ReadData(BinaryReader reader, int size, SampleFormat sampleFormat, int channelCount)
         {
             if (sampleFormat == 0)
             {
@@ -143,15 +196,15 @@ namespace NumFlat.IO
             var buffer = ubuffer.Memory.Span.Slice(0, size);
             reader.Read(buffer);
 
-            var sampleCount = size / (GetSampleSize(sampleFormat) * channels);
+            var sampleCount = size / (GetSampleSize(sampleFormat) * channelCount);
 
-            var data = new Vec<double>[channels];
-            for (var ch = 0; ch < channels; ch++)
+            var data = new Vec<double>[channelCount];
+            for (var ch = 0; ch < channelCount; ch++)
             {
                 data[ch] = new Vec<double>(sampleCount);
             }
 
-            for (var ch = 0; ch < channels; ch++)
+            for (var ch = 0; ch < channelCount; ch++)
             {
                 if (sampleFormat == SampleFormat.Int16)
                 {
@@ -161,7 +214,7 @@ namespace NumFlat.IO
                     for (var i = 0; i < dst.Length; i++)
                     {
                         dst[i] = (double)src[position] / 32768;
-                        position += channels;
+                        position += channelCount;
                     }
                 }
                 else if (sampleFormat == SampleFormat.Float32)
@@ -172,13 +225,58 @@ namespace NumFlat.IO
                     for (var i = 0; i < dst.Length; i++)
                     {
                         dst[i] = src[position];
-                        position += channels;
+                        position += channelCount;
                     }
                 }
                 else
                 {
                     throw new InvalidDataException($"Unsupported sample format ({(int)sampleFormat}).");
                 }
+            }
+
+            return data;
+        }
+
+        private static Vec<double> ReadDataSingleChannel(BinaryReader reader, int size, SampleFormat sampleFormat, int channelCount, int channel)
+        {
+            if (sampleFormat == 0)
+            {
+                throw new InvalidDataException($"The format chunk was not found.");
+            }
+
+            using var ubuffer = MemoryPool<byte>.Shared.Rent(size);
+            var buffer = ubuffer.Memory.Span.Slice(0, size);
+            reader.Read(buffer);
+
+            var sampleCount = size / (GetSampleSize(sampleFormat) * channelCount);
+
+            var data = new Vec<double>(sampleCount);
+
+            if (sampleFormat == SampleFormat.Int16)
+            {
+                var src = MemoryMarshal.Cast<byte, short>(buffer);
+                var dst = data.Memory.Span;
+                var position = channel;
+                for (var i = 0; i < dst.Length; i++)
+                {
+                    dst[i] = (double)src[position] / 32768;
+                    position += channelCount;
+                }
+            }
+            else if (sampleFormat == SampleFormat.Float32)
+            {
+                var src = MemoryMarshal.Cast<byte, float>(buffer);
+                var dst = data.Memory.Span;
+                var position = channel;
+                for (var i = 0; i < dst.Length; i++)
+                {
+                    dst[i] = src[position];
+                    position += channelCount;
+                }
+            }
+            else
+            {
+                throw new InvalidDataException($"Unsupported sample format ({(int)sampleFormat}).");
             }
 
             return data;
--- NumFlatTest/IOTests/WaveFileTests.cs
+++ NumFlatTest/IOTests/WaveFileTests.cs
@@ -13,7 +13,7 @@ namespace NumFlatTest.IOTests
     public class WaveFileTests
     {
         [Test]
-        public void Sin1000Hz_16bit()
+        public void Read_Sin1000Hz_16bit()
         {
             var (data, sampleRate) = WaveFile.Read(Path.Combine("dataset", "sin1000hz_16bit.wav"));
 
@@ -30,7 +30,7 @@ namespace NumFlatTest.IOTests
         }
 
         [Test]
-        public void Sin1000Hz_32bit()
+        public void Read_Sin1000Hz_32bit()
         {
             var (data, sampleRate) = WaveFile.Read(Path.Combine("dataset", "sin1000hz_32bit.wav"));
 
@@ -47,7 +47,7 @@ namespace NumFlatTest.IOTests
         }
 
         [Test]
-        public void Sin1000Hz2000Hz_16bit()
+        public void Read_Sin1000Hz2000Hz_16bit()
         {
             var (data, sampleRate) = WaveFile.Read(Path.Combine("dataset", "sin1000hz2000hz_16bit.wav"));
 
@@ -72,7 +72,7 @@ namespace NumFlatTest.IOTests
         }
 
         [Test]
-        public void Sin1000Hz2000Hz_32bit()
+        public void Read_Sin1000Hz2000Hz_32bit()
         {
             var (data, sampleRate) = WaveFile.Read(Path.Combine("dataset", "sin1000hz2000hz_32bit.wav"));
 
@@ -92,6 +92,90 @@ namespace NumFlatTest.IOTests
             {
                 var actual = data[1][i];
                 var expected = 0.5 * Math.Sin(2 * Math.PI * i / sampleRate * 2000);
+                Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
+            }
+        }
+
+        [Test]
+        public void ReadMono_Sin1000Hz_16bit()
+        {
+            var (data, sampleRate) = WaveFile.ReadMono(Path.Combine("dataset", "sin1000hz_16bit.wav"));
+
+            Assert.That(data.Count == 1600);
+            Assert.That(sampleRate == 16000);
+
+            for (var i = 0; i < 1600; i++)
+            {
+                var actual = data[i];
+                var expected = 0.5 * Math.Sin(2 * Math.PI * i / sampleRate * 1000);
+                Assert.That(actual, Is.EqualTo(expected).Within(1.0E-4));
+            }
+        }
+
+        [Test]
+        public void ReadMono_Sin1000Hz_32bit()
+        {
+            var (data, sampleRate) = WaveFile.ReadMono(Path.Combine("dataset", "sin1000hz_32bit.wav"));
+
+            Assert.That(data.Count == 1600);
+            Assert.That(sampleRate == 16000);
+
+            for (var i = 0; i < 1600; i++)
+            {
+                var actual = data[i];
+                var expected = 0.5 * Math.Sin(2 * Math.PI * i / sampleRate * 1000);
+                Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
+            }
+        }
+
+        [Test]
+        public void ReadMono_Sin1000Hz2000Hz_16bit()
+        {
+            var (data1, sampleRate1) = WaveFile.ReadMono(Path.Combine("dataset", "sin1000hz2000hz_16bit.wav"), 0);
+            var (data2, sampleRate2) = WaveFile.ReadMono(Path.Combine("dataset", "sin1000hz2000hz_16bit.wav"), 1);
+
+            Assert.That(data1.Count == 1600);
+            Assert.That(data2.Count == 1600);
+            Assert.That(sampleRate1 == 16000);
+            Assert.That(sampleRate2 == 16000);
+
+            for (var i = 0; i < 1600; i++)
+            {
+                var actual = data1[i];
+                var expected = 0.5 * Math.Sin(2 * Math.PI * i / sampleRate1 * 1000);
+                Assert.That(actual, Is.EqualTo(expected).Within(1.0E-4));
+            }
+
+            for (var i = 0; i < 1600; i++)
+            {
+                var actual = data2[i];
+                var expected = 0.5 * Math.Sin(2 * Math.PI * i / sampleRate2 * 2000);
+                Assert.That(actual, Is.EqualTo(expected).Within(1.0E-4));
+            }
+        }
+
+        [Test]
+        public void ReadMono_Sin1000Hz2000Hz_32bit()
+        {
+            var (data1, sampleRate1) = WaveFile.ReadMono(Path.Combine("dataset", "sin1000hz2000hz_32bit.wav"), 0);
+            var (data2, sampleRate2) = WaveFile.ReadMono(Path.Combine("dataset", "sin1000hz2000hz_32bit.wav"), 1);
+
+            Assert.That(data1.Count == 1600);
+            Assert.That(data2.Count == 1600);
+            Assert.That(sampleRate1 == 16000);
+            Assert.That(sampleRate2 == 16000);
+
+            for (var i = 0; i < 1600; i++)
+            {
+                var actual = data1[i];
+                var expected = 0.5 * Math.Sin(2 * Math.PI * i / sampleRate1 * 1000);
+                Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
+            }
+
+            for (var i = 0; i < 1600; i++)
+            {
+                var actual = data2[i];
+                var expected = 0.5 * Math.Sin(2 * Math.PI * i / sampleRate2 * 2000);
                 Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
             }
         }
