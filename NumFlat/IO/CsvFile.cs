027b22a0069be8bd2c1cfcaaa33b2a89b67a7614
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-03 20:44:33 +0900

    Add CSV file IO.

---
+++ NumFlat/IO/CsvFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NumFlat.IO
+{
+    /// <summary>
+    /// Provides CSV file IO.
+    /// </summary>
+    public static class CsvFile
+    {
+        /// <summary>
+        /// Reads a CSV file as a <see cref="Mat{T}"/>.
+        /// </summary>
+        /// <param name="path">
+        /// The path of the CSV file.
+        /// </param>
+        /// <param name="skipHeader">
+        /// The number of lines at the beginning to skip.
+        /// </param>
+        /// <returns>
+        /// An instance of <see cref="Mat{T}"/>.
+        /// </returns>
+        public static Mat<double> Read(string path, int skipHeader = 0)
+        {
+            ThrowHelper.ThrowIfNull(path, nameof(path));
+
+            var buffer = new List<IEnumerable<double>>();
+            foreach (var line in File.ReadLines(path).Skip(skipHeader))
+            {
+                var split = line.Split(',');
+                buffer.Add(split.Select(double.Parse));
+            }
+            return buffer.RowsToMatrix();
+        }
+
+        /// <summary>
+        /// Writes a <see cref="Mat{T}"/> as a CSV file.
+        /// </summary>
+        /// <param name="path">
+        /// The path of the CSV file.
+        /// </param>
+        /// <param name="mat">
+        /// The <see cref="Mat{T}"/> to be written.
+        /// </param>
+        public static void Write(string path, in Mat<double> mat)
+        {
+            ThrowHelper.ThrowIfNull(path, nameof(path));
+            ThrowHelper.ThrowIfEmpty(mat, nameof(mat));
+
+            using (var writer = new StreamWriter(path))
+            {
+                foreach (var row in mat.Rows)
+                {
+                    writer.WriteLine(string.Join(',', row));
+                }
+            }
+        }
+    }
+}
---
+++ NumFlatTest/IOTests/CsvFileTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics;
+using NUnit.Framework;
+using NumFlat;
+using NumFlat.IO;
+
+namespace NumFlatTest.IOTests
+{
+    public class CsvFileTests
+    {
+        [Test]
+        public void Read()
+        {
+            var expected = new double[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+                { 10, 11, 12 },
+            }
+            .ToMatrix();
+
+            var actual = CsvFile.Read(Path.Combine("dataset", "simple.csv"), 1);
+
+            NumAssert.AreSame(expected, actual, 0);
+        }
+
+        [Test]
+        public void Write()
+        {
+            var expected = new double[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+                { 10, 11, 12 },
+            }
+            .ToMatrix();
+
+            CsvFile.Write("test.csv", expected);
+
+            var actual = CsvFile.Read("test.csv");
+
+            NumAssert.AreSame(expected, actual, 0);
+        }
+    }
+}
