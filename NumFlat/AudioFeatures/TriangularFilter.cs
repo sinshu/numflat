eea4a6b572f8e09e9c393d02b4451d18fd683883
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-04-13 18:34:39 +0900

    Start audio feature extraction.

---
+++ NumFlat/AudioFeatures/TriangularFilter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace NumFlat.AudioFeatures
+{
+    public sealed class TriangularFilter
+    {
+        private readonly int sampleRate;
+        private readonly int fftLength;
+        private readonly double lowerFrequency;
+        private readonly double centerFrequency;
+        private readonly double upperFrequency;
+
+        private readonly int startFrequencyBinIndex;
+        private readonly Vec<double> coefficients;
+
+        public TriangularFilter(int sampleRate, int fftLength, double lowerFrequency, double centerFrequency, double upperFrequency)
+        {
+            if (sampleRate <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sampleRate), "The sample rate must be a positive value.");
+            }
+
+            if (fftLength <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(fftLength), "The FFT length must be a positive value.");
+            }
+
+            if (lowerFrequency < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lowerFrequency), "The frequency value must be a non-negative value.");
+            }
+
+            if (centerFrequency < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(centerFrequency), "The frequency value must be a non-negative value.");
+            }
+
+            if (upperFrequency < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(upperFrequency), "The frequency value must be a non-negative value.");
+            }
+
+            if (lowerFrequency > centerFrequency)
+            {
+                throw new ArgumentException("The lower frequency must be smaller than or equal to the center frequency.");
+            }
+
+            if (upperFrequency < centerFrequency)
+            {
+                throw new ArgumentException("The upper frequency must be greater than or equal to the center frequency.");
+            }
+
+            this.sampleRate = sampleRate;
+            this.fftLength = fftLength;
+            this.lowerFrequency = lowerFrequency;
+            this.centerFrequency = centerFrequency;
+            this.upperFrequency = upperFrequency;
+
+            var lowerW = lowerFrequency / sampleRate * fftLength;
+            var centerW = centerFrequency / sampleRate * fftLength;
+            var upperW = upperFrequency / sampleRate * fftLength;
+
+            var startFrequencyBinIndex = (int)Math.Floor(lowerW);
+            var endFrequencyBinIndex = (int)Math.Ceiling(upperW);
+            var frequencyBinCount = endFrequencyBinIndex - startFrequencyBinIndex;
+
+            var useLowerPart = centerW - lowerW > 0;
+            var useUpperPart = upperW - centerW > 0;
+
+            var coefficients = new Vec<double>(frequencyBinCount);
+            var fc = coefficients.GetUnsafeFastIndexer();
+            for (var i = 0; i < coefficients.Count; i++)
+            {
+                var w = startFrequencyBinIndex + i;
+
+                if (useLowerPart && w <= (int)centerW)
+                {
+                    var x1 = Math.Max(w, lowerW);
+                    var x2 = Math.Min(w + 1, centerW);
+                    var y1 = (x1 - lowerW) / (centerW - lowerW);
+                    var y2 = (x2 - lowerW) / (centerW - lowerW);
+                    fc[i] += (y1 + y2) * (x2 - x1) / 2;
+                }
+
+                if (useUpperPart && w >= (int)centerW)
+                {
+                    var x1 = Math.Max(w, centerW);
+                    var x2 = Math.Min(w + 1, upperW);
+                    var y1 = (upperW - x1) / (upperW - centerW);
+                    var y2 = (upperW - x2) / (upperW - centerW);
+                    fc[i] += (y1 + y2) * (x2 - x1) / 2;
+                }
+            }
+
+            this.startFrequencyBinIndex = startFrequencyBinIndex;
+            this.coefficients = coefficients;
+        }
+
+        public int SampleRate => sampleRate;
+        public int FftLength => fftLength;
+        public double LowerFrequency => lowerFrequency;
+        public double CenterFrequency => centerFrequency;
+        public double UpperFrequency => upperFrequency;
+        public int StartFrequencyBinIndex => startFrequencyBinIndex;
+        public ref readonly Vec<double> Coefficients => ref coefficients;
+    }
+}
--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using NumFlat;
+using NumFlat.AudioFeatures;
 using NumFlat.Clustering;
 using NumFlat.Distributions;
 using NumFlat.IO;
@@ -17,5 +18,56 @@ public class Program
         VectorExamples.Run();
         MatrixExamples.Run();
         OtherExamples.Run();
+
+        var sampleRate = 16000;
+        var fftLength = 1024;
+        var filters = new TriangularFilter[10];
+        var frequencies = new double[filters.Length + 2];
+        var min = 50;
+        var max = 700;
+        for (var i = 0; i < frequencies.Length; i++)
+        {
+            frequencies[i] = min + (double)i / (frequencies.Length - 1) * (max - min);
+        }
+        for (var i = 0; i < filters.Length; i++)
+        {
+            var lower = frequencies[i];
+            var center = frequencies[i + 1];
+            var upper = frequencies[i + 2];
+            if (i == 3)
+            {
+                Console.WriteLine();
+            }
+            filters[i] = new TriangularFilter(sampleRate, fftLength, lower, center, upper);
+        }
+
+        using (var writer = new StreamWriter("test.csv"))
+        {
+            var sep = ",";
+            foreach (var filter in filters)
+            {
+                for (var i = 0; i < filter.Coefficients.Count; i++)
+                {
+                    writer.WriteLine((filter.StartFrequencyBinIndex + i) + sep + filter.Coefficients[i]);
+                }
+                sep += ",";
+            }
+        }
+
+        var a = new double[fftLength / 2];
+        foreach (var filter in filters)
+        {
+            for (var i = 0; i < filter.Coefficients.Count; i++)
+            {
+                a[filter.StartFrequencyBinIndex + i] += filter.Coefficients[i];
+            }
+        }
+        var k = 0;
+        foreach (var value in a)
+        {
+            var freq = (double)k / fftLength * sampleRate;
+            Console.WriteLine(value + " " + freq);
+            k++;
+        }
     }
 }
