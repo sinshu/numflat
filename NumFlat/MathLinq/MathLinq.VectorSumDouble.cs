cb8cf6b2996d1d9bb99a6a270fbbc8e84bd5799c
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-05-01 21:43:29 +0900

    Add vector sum.

---
+++ NumFlat/MathLinq/MathLinq.VectorSumDouble.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace NumFlat
+{
+    public static partial class MathLinq
+    {
+        /// <summary>
+        /// Computes the vector sum from a sequence of vectors.
+        /// </summary>
+        /// <param name="xs">
+        /// The source vectors.
+        /// </param>
+        /// <param name="destination">
+        /// The destination of the vector sum.
+        /// </param>
+        public static void Sum(IEnumerable<Vec<double>> xs, in Vec<double> destination)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfEmpty(destination, nameof(destination));
+
+            destination.Clear();
+
+            foreach (var x in xs)
+            {
+                if (x.Count != destination.Count)
+                {
+                    throw new ArgumentException("All the source vectors must have the same length as the destination.");
+                }
+
+                destination.AddInplace(x);
+            }
+        }
+
+        /// <summary>
+        /// Computes the vector sum from a sequence of vectors.
+        /// </summary>
+        /// <param name="xs">
+        /// The source vectors.
+        /// </param>
+        /// <returns>
+        /// The vector sum.
+        /// </returns>
+        public static Vec<double> Sum(this IEnumerable<Vec<double>> xs)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+
+            var destination = new Vec<double>();
+
+            foreach (var x in xs)
+            {
+                if (destination.Count == 0)
+                {
+                    if (x.Count == 0)
+                    {
+                        new ArgumentException("Empty vectors are not allowed.");
+                    }
+
+                    destination = new Vec<double>(x.Count);
+                }
+
+                if (x.Count != destination.Count)
+                {
+                    throw new ArgumentException("All the vectors must have the same length.");
+                }
+
+                destination.AddInplace(x);
+            }
+
+            if (destination.Count == 0)
+            {
+                throw new ArgumentException("The sequence must contain at least one vector.");
+            }
+
+            return destination;
+        }
+    }
+}
---
+++ NumFlatTest/MathLinqTests/VectorSumTestsDouble.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics.Statistics;
+using NUnit.Framework;
+using NumFlat;
+
+using MVec = MathNet.Numerics.LinearAlgebra.Vector<double>;
+using MMat = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+
+namespace NumFlatTest.MathLinqTests
+{
+    public class VectorSumTestsDouble
+    {
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, 3)]
+        [TestCase(1, 3, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 1)]
+        [TestCase(3, 1, 4)]
+        [TestCase(3, 10, 1)]
+        [TestCase(3, 10, 2)]
+        [TestCase(5, 20, 3)]
+        public void Sum(int dim, int count, int dstStride)
+        {
+            var data = CreateData(42, dim, count);
+            var expected = MathNetSum(data);
+
+            var actual = TestVector.RandomDouble(0, dim, dstStride);
+            MathLinq.Sum(data.Select(x => x.ToVector()), actual);
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+
+            TestVector.FailIfOutOfRangeWrite(actual);
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(1, 3)]
+        [TestCase(3, 1)]
+        [TestCase(3, 10)]
+        [TestCase(5, 20)]
+        public void Sum_ExtensionMethod(int dim, int count)
+        {
+            var data = CreateData(42, dim, count);
+            var expected = MathNetSum(data);
+            var actual = data.Select(x => x.ToVector()).Sum();
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+        }
+
+        private static MVec MathNetSum(IEnumerable<MVec> xs)
+        {
+            MVec sum = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(xs.First().Count);
+            foreach (var x in xs)
+            {
+                sum += x;
+            }
+            return sum;
+        }
+
+        private static MVec[] CreateData(int seed, int dim, int count)
+        {
+            var random = new Random(seed);
+
+            var data = new List<MVec>();
+
+            for (var i = 0; i < count; i++)
+            {
+                var elements = Enumerable.Range(0, dim).Select(i => random.NextDouble()).ToArray();
+                var x = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(elements);
+                data.Add(x);
+            }
+
+            return data.ToArray();
+        }
+    }
+}
