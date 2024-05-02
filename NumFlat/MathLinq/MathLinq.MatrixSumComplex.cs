ccce01c77d4915cb3d97e49eed4f8c7ba0d72f0b
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-05-02 11:51:20 +0900

    Add matrix sum.

---
+++ NumFlat/MathLinq/MathLinq.MatrixSumComplex.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace NumFlat
+{
+    public static partial class MathLinq
+    {
+        /// <summary>
+        /// Computes the matrix sum from a sequence of matrices.
+        /// </summary>
+        /// <param name="xs">
+        /// The source matrices.
+        /// </param>
+        /// <param name="destination">
+        /// The destination of the matrix sum.
+        /// </param>
+        public static void Sum(IEnumerable<Mat<Complex>> xs, in Mat<Complex> destination)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfEmpty(destination, nameof(destination));
+
+            destination.Clear();
+
+            foreach (var x in xs)
+            {
+                if (x.RowCount != destination.RowCount || x.ColCount != destination.ColCount)
+                {
+                    throw new ArgumentException("All the source matrices must have the same dimensions as the destination.");
+                }
+
+                destination.AddInplace(x);
+            }
+        }
+
+        /// <summary>
+        /// Computes the matrix sum from a sequence of matrices.
+        /// </summary>
+        /// <param name="xs">
+        /// The source matrices.
+        /// </param>
+        /// <returns>
+        /// The matrix sum.
+        /// </returns>
+        public static Mat<Complex> Sum(this IEnumerable<Mat<Complex>> xs)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+
+            var destination = new Mat<Complex>();
+
+            foreach (var x in xs)
+            {
+                if (destination.RowCount == 0)
+                {
+                    if (x.RowCount == 0 || x.ColCount == 0)
+                    {
+                        new ArgumentException("Empty matrices are not allowed.");
+                    }
+
+                    destination = new Mat<Complex>(x.RowCount, x.ColCount);
+                }
+
+                if (x.RowCount != destination.RowCount || x.ColCount != destination.ColCount)
+                {
+                    throw new ArgumentException("All the matrices must have the same dimensions.");
+                }
+
+                destination.AddInplace(x);
+            }
+
+            if (destination.RowCount == 0)
+            {
+                throw new ArgumentException("The sequence must contain at least one matrix.");
+            }
+
+            return destination;
+        }
+
+        /// <summary>
+        /// Computes the matrix sum from a sequence of matrices and their weights.
+        /// </summary>
+        /// <param name="xs">
+        /// The source matrices.
+        /// </param>
+        /// <param name="weights">
+        /// The weights of the source matrices.
+        /// </param>
+        /// <param name="destination">
+        /// The destination of the weighted matrix sum.
+        /// </param>
+        public static void Sum(IEnumerable<Mat<Complex>> xs, IEnumerable<double> weights, in Mat<Complex> destination)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfNull(weights, nameof(weights));
+            ThrowHelper.ThrowIfEmpty(destination, nameof(destination));
+
+            destination.Clear();
+
+            using (var exs = xs.GetEnumerator())
+            using (var eweights = weights.GetEnumerator())
+            {
+                while (true)
+                {
+                    var xsHasNext = exs.MoveNext();
+                    var weightsHasNext = eweights.MoveNext();
+                    if (xsHasNext != weightsHasNext)
+                    {
+                        throw new ArgumentException("The number of source matrices and weights must match.");
+                    }
+
+                    if (xsHasNext)
+                    {
+                        var x = exs.Current;
+                        var w = eweights.Current;
+
+                        if (x.RowCount != destination.RowCount || x.ColCount != destination.ColCount)
+                        {
+                            throw new ArgumentException("All the source matrices must have the same dimensions as the destination.");
+                        }
+
+                        if (w < 0)
+                        {
+                            throw new ArgumentException("Negative weight values are not allowed.");
+                        }
+
+                        AccumulateWeightedSum(x, w, destination);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Computes the matrix sum from a sequence of matrices and their weights.
+        /// </summary>
+        /// <param name="xs">
+        /// The source matrices.
+        /// </param>
+        /// <param name="weights">
+        /// The weights of the source matrices.
+        /// </param>
+        /// <returns>
+        /// The weighted matrix sum.
+        /// </returns>
+        public static Mat<Complex> Sum(this IEnumerable<Mat<Complex>> xs, IEnumerable<double> weights)
+        {
+            ThrowHelper.ThrowIfNull(xs, nameof(xs));
+            ThrowHelper.ThrowIfNull(weights, nameof(weights));
+
+            var destination = new Mat<Complex>();
+
+            using (var exs = xs.GetEnumerator())
+            using (var eweights = weights.GetEnumerator())
+            {
+
+                while (true)
+                {
+                    var xsHasNext = exs.MoveNext();
+                    var weightsHasNext = eweights.MoveNext();
+                    if (xsHasNext != weightsHasNext)
+                    {
+                        throw new ArgumentException("The number of matrices and weights must match.");
+                    }
+
+                    if (xsHasNext)
+                    {
+                        var x = exs.Current;
+                        var w = eweights.Current;
+
+                        if (destination.RowCount == 0)
+                        {
+                            if (x.RowCount == 0 || x.ColCount == 0)
+                            {
+                                new ArgumentException("Empty matrices are not allowed.");
+                            }
+
+                            destination = new Mat<Complex>(x.RowCount, x.ColCount);
+                        }
+
+                        if (x.RowCount != destination.RowCount || x.ColCount != destination.ColCount)
+                        {
+                            throw new ArgumentException("All the matrices must have the same dimensions.");
+                        }
+
+                        if (w < 0)
+                        {
+                            throw new ArgumentException("Negative weight values are not allowed.");
+                        }
+
+                        AccumulateWeightedSum(x, w, destination);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            if (destination.RowCount == 0)
+            {
+                throw new ArgumentException("The sequence must contain at least one matrix.");
+            }
+
+            return destination;
+        }
+    }
+}
---
+++ NumFlatTest/MathLinqTests/MatrixSumTestsComplex.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics.Statistics;
+using NUnit.Framework;
+using NumFlat;
+
+using MVec = MathNet.Numerics.LinearAlgebra.Vector<System.Numerics.Complex>;
+using MMat = MathNet.Numerics.LinearAlgebra.Matrix<System.Numerics.Complex>;
+
+namespace NumFlatTest.MathLinqTests
+{
+    public class MatrixSumTestsComplex
+    {
+        [TestCase(1, 1, 1, 2)]
+        [TestCase(1, 1, 3, 2)]
+        [TestCase(3, 2, 1, 4)]
+        [TestCase(3, 2, 3, 4)]
+        [TestCase(4, 5, 6, 5)]
+        [TestCase(3, 6, 4, 4)]
+        public void Sum(int rowCount, int colCount, int matCount, int dstStride)
+        {
+            var data = CreateData(42, rowCount, colCount, matCount);
+            var expected = MathNetSum(data);
+
+            var actual = TestMatrix.RandomComplex(0, rowCount, colCount, dstStride);
+            MathLinq.Sum(data.Select(x => x.ToArray().ToMatrix()), actual);
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+
+            TestMatrix.FailIfOutOfRangeWrite(actual);
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, 3)]
+        [TestCase(3, 2, 1)]
+        [TestCase(3, 2, 3)]
+        [TestCase(4, 5, 6)]
+        [TestCase(3, 6, 4)]
+        public void Sum_ExtensionMethod(int rowCount, int colCount, int matCount)
+        {
+            var data = CreateData(42, rowCount, colCount, matCount);
+            var expected = MathNetSum(data);
+            var actual = data.Select(x => x.ToArray().ToMatrix()).Sum();
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+        }
+
+        [TestCase(1, 1, 1, 2)]
+        [TestCase(1, 1, 3, 2)]
+        [TestCase(3, 2, 1, 4)]
+        [TestCase(3, 2, 3, 4)]
+        [TestCase(4, 5, 6, 5)]
+        [TestCase(3, 6, 4, 4)]
+        public void WeightedSum(int rowCount, int colCount, int matCount, int dstStride)
+        {
+            var data = CreateData(42, rowCount, colCount, matCount);
+            var random = new Random(57);
+            var weights = data.Select(x => random.NextDouble()).ToArray();
+            var expected = MathNetSum(data, weights);
+
+            var actual = TestMatrix.RandomComplex(0, rowCount, colCount, dstStride);
+            MathLinq.Sum(data.Select(x => x.ToArray().ToMatrix()), weights, actual);
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+
+            TestMatrix.FailIfOutOfRangeWrite(actual);
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, 3)]
+        [TestCase(3, 2, 1)]
+        [TestCase(3, 2, 3)]
+        [TestCase(4, 5, 6)]
+        [TestCase(3, 6, 4)]
+        public void WeightedSum_ExtensionMethod(int rowCount, int colCount, int matCount)
+        {
+            var data = CreateData(42, rowCount, colCount, matCount);
+            var random = new Random(57);
+            var weights = data.Select(x => random.NextDouble()).ToArray();
+            var expected = MathNetSum(data, weights);
+
+            var actual = data.Select(x => x.ToArray().ToMatrix()).Sum(weights);
+
+            NumAssert.AreSame(expected, actual, 1.0E-12);
+        }
+
+        private static MMat MathNetSum(IEnumerable<MMat> xs)
+        {
+            var first = xs.First();
+            MMat sum = new MathNet.Numerics.LinearAlgebra.Complex.DenseMatrix(first.RowCount, first.ColumnCount);
+            foreach (var x in xs)
+            {
+                sum += x;
+            }
+            return sum;
+        }
+
+        private static MMat MathNetSum(IEnumerable<MMat> xs, IEnumerable<double> weights)
+        {
+            var first = xs.First();
+            MMat sum = new MathNet.Numerics.LinearAlgebra.Complex.DenseMatrix(first.RowCount, first.ColumnCount);
+            foreach (var (w, x) in weights.Zip(xs))
+            {
+                sum += w * x;
+            }
+            return sum;
+        }
+
+        private static MMat[] CreateData(int seed, int rowCount, int colCount, int matCount)
+        {
+            var random = new Random(seed);
+
+            var data = new List<MMat>();
+
+            for (var i = 0; i < matCount; i++)
+            {
+                var x = new MathNet.Numerics.LinearAlgebra.Complex.DenseMatrix(rowCount, colCount);
+                for (var row = 0; row < rowCount; row++)
+                {
+                    for (var col = 0; col < colCount; col++)
+                    {
+                        x[row, col] = new Complex(random.NextDouble(), random.NextDouble());
+                    }
+                }
+                data.Add(x);
+            }
+
+            return data.ToArray();
+        }
+    }
+}
