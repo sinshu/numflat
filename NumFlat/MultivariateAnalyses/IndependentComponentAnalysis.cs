ee471dc564a83f890dcde47934ee80444cf98b77
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-06-19 19:47:22 +0900

    Start ICA optimization.

--- NumFlat/MultivariateAnalyses/IndependentComponentAnalysis.cs
+++ NumFlat/MultivariateAnalyses/IndependentComponentAnalysis.cs
@@ -15,50 +15,58 @@ namespace NumFlat.MultivariateAnalyses
             this.pca = xs.Pca();
             this.componentCount = componentCount;
 
+            using var uwhiten = new TemporalMatrix<double>(xs[0].Count, xs.Count);
+            ref readonly var whiten = ref uwhiten.Item;
+
+            using var uw2 = new TemporalMatrix<double>(componentCount, componentCount);
+            ref readonly var w2 = ref uw2.Item;
+
+            using var utransformed = new TemporalMatrix<double>(xs[0].Count, xs.Count);
+            ref readonly var transformed = ref utransformed.Item;
+
             var scale = pca.EigenValues.Map(Math.Sqrt);
-            var whiten = xs.Select(x => pca.Transform(x).PointwiseDiv(scale)).ToArray();
 
-            Console.WriteLine(whiten.Covariance());
-
-            xs = whiten;
+            foreach (var (x, a) in xs.Zip(whiten.Cols))
+            {
+                pca.Transform(x, a);
+                a.PointwiseDivInplace(scale);
+            }
+            Console.WriteLine(whiten.Cols.Covariance());
 
             // Vectors stored as rows.
             // Each row corresponds to a component.
-            var random = new Random(57);
-            var w = new Mat<double>(componentCount, componentCount);
-            foreach (ref var value in w.Memory.Span)
+            var random = new Random(42);
+            var w1 = new Mat<double>(componentCount, componentCount);
+            foreach (ref var value in w1.Memory.Span)
             {
                 value = 2 * random.NextDouble() - 1;
             }
-            Console.WriteLine(w);
 
             Console.ReadKey();
 
             for (var ite = 0; ite < 30; ite++)
             {
-                var svd = w.Svd();
-                var u = svd.U;
-                var sinv = svd.S.Select(s => 1 / s).ToDiagonalMatrix();
-                var k = u * sinv * u.Transpose();
-                w = k * w;
-                Console.WriteLine(w);
+                Orthogonalize(w1, w2);
+                w2.CopyTo(w1);
 
                 for (var c = 0; c < componentCount; c++)
                 {
-                    var wc = w.Rows[c];
-                    var wxs = xs.Select(x => wc * x).ToArray();
+                    var wc = w1.Rows[c];
+                    var wxs = whiten.Cols.Select(x => wc * x).ToArray();
                     var gwxs = wxs.Select(G).ToArray();
                     var gpwxs = wxs.Select(Gp).ToArray();
-                    var e1 = xs.Zip(gwxs, (x, g) => x * g).Mean();
+                    var e1 = whiten.Cols.Zip(gwxs, (x, g) => x * g).Mean();
                     var e2 = gpwxs.Average();
                     var wc2 = e1 - e2 * wc;
                     wc2.CopyTo(wc);
                 }
+
+                Console.WriteLine(ite);
             }
 
             using (var writer = new StreamWriter("ica.csv"))
             {
-                foreach (var y in xs.Select(x => w * x))
+                foreach (var y in whiten.Cols.Select(x => w1 * x))
                 {
                     writer.WriteLine(string.Join(',', y));
                 }
@@ -67,6 +75,27 @@ namespace NumFlat.MultivariateAnalyses
 
         public void Transform(in Vec<double> source, in Vec<double> destination)
         {
+        }
+
+        private static void Orthogonalize(in Mat<double> source, in Mat<double> destination)
+        {
+            using var us = new TemporalVector<double>(source.RowCount);
+            ref readonly var s = ref us.Item;
+            var fs = s.GetUnsafeFastIndexer();
+
+            using var uk = new TemporalMatrix<double>(source.RowCount, source.ColCount);
+            ref readonly var k = ref uk.Item;
+
+            using var utmp = new TemporalMatrix<double>(source.RowCount, source.ColCount);
+            ref readonly var tmp = ref utmp.Item;
+
+            SingularValueDecompositionDouble.Decompose(source, s, destination);
+            for (var col = 0; col < destination.ColCount; col++)
+            {
+                Vec.Div(destination.Cols[col], fs[col], tmp.Cols[col]);
+            }
+            Mat.Mul(tmp, destination, k, false, true);
+            Mat.Mul(k, source, destination, false, false);
         }
 
         /// <inheritdoc/>
