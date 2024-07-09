7339149ecf241c9fdc11994c9f5fe289def70d0a
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-07-09 21:56:07 +0900

    Improve random generator tests.

--- NumFlatTest/RandomExtensionsTest.cs
+++ NumFlatTest/RandomExtensionsTest.cs
@@ -17,8 +17,12 @@ namespace NumFlatTest
             var random = new Random(seed);
             var values = Enumerable.Range(0, 1000).Select(i => random.NextGaussian()).ToArray();
             var (mean, sd) = values.MeanAndStandardDeviation();
+            var skewness = values.Skewness();
+            var kurtosis = values.Kurtosis();
             Assert.That(mean, Is.EqualTo(0.0).Within(0.1));
             Assert.That(sd, Is.EqualTo(1.0).Within(0.1));
+            Assert.That(skewness, Is.EqualTo(0.0).Within(0.2));
+            Assert.That(kurtosis, Is.EqualTo(0.0).Within(0.4));
         }
 
         [TestCase(42)]
@@ -31,14 +35,22 @@ namespace NumFlatTest
 
             {
                 var (mean, sd) = values.Select(x => x.Real).MeanAndStandardDeviation();
+                var skewness = values.Select(x => x.Real).Skewness();
+                var kurtosis = values.Select(x => x.Real).Kurtosis();
                 Assert.That(mean, Is.EqualTo(0.0).Within(0.1));
                 Assert.That(sd, Is.EqualTo(1.0).Within(0.1));
+                Assert.That(skewness, Is.EqualTo(0.0).Within(0.2));
+                Assert.That(kurtosis, Is.EqualTo(0.0).Within(0.4));
             }
 
             {
                 var (mean, sd) = values.Select(x => x.Imaginary).MeanAndStandardDeviation();
+                var skewness = values.Select(x => x.Imaginary).Skewness();
+                var kurtosis = values.Select(x => x.Imaginary).Kurtosis();
                 Assert.That(mean, Is.EqualTo(0.0).Within(0.1));
                 Assert.That(sd, Is.EqualTo(1.0).Within(0.1));
+                Assert.That(skewness, Is.EqualTo(0.0).Within(0.2));
+                Assert.That(kurtosis, Is.EqualTo(0.0).Within(0.4));
             }
         }
     }
