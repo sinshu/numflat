9aded1afdda494c9d12dcbdd28d1dfd536acc6ce
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-08-03 14:00:10 +0900

    Try logistic regression.

---
+++ NumFlat/MultivariateAnalyses/LogisticRegression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumFlat.MultivariateAnalyses
+{
+    public sealed class LogisticRegression
+    {
+        public LogisticRegression(IEnumerable<Vec<double>> xs, IEnumerable<int> ys)
+        {
+
+        }
+    }
+}
