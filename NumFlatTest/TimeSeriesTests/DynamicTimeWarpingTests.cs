a9fb01176461fdde04880a234c346a2525a8a457
Nobuaki Tanaka <sinshu.d@gmail.com>
2025-04-06 19:14:01 +0900

    Add more tests.

--- NumFlatTest/TimeSeriesTests/DynamicTimeWarpingTests.cs
+++ NumFlatTest/TimeSeriesTests/DynamicTimeWarpingTests.cs
@@ -10,6 +10,40 @@ namespace NumFlatTest.TimeSeriesTests
 {
     public class DynamicTimeWarpingTests
     {
+        [Test]
+        public void Zero()
+        {
+            int[] xs = [1, 1, 1, 2, 2, 2, 3, 3, 3, 1, 1, 1, 2, 2, 2, 3, 3, 3];
+            int[] ys = [1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3];
+
+            {
+                var distance = DynamicTimeWarping.GetDistance(xs, ys, new IntAbs());
+                Assert.That(distance, Is.EqualTo(0));
+            }
+
+            {
+                var distance = DynamicTimeWarping.GetDistance(ys, xs, new IntAbs());
+                Assert.That(distance, Is.EqualTo(0));
+            }
+        }
+
+        [Test]
+        public void One()
+        {
+            int[] xs = [1, 1, 1, 2, 2, 2, 3, 3, 3, 1, 1, 1, 2, 2, 2, 3, 3, 3];
+            int[] ys = [1, 1, 1, 1, 2, 2, 3, 3, 4, 3, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3];
+
+            {
+                var distance = DynamicTimeWarping.GetDistance(xs, ys, new IntAbs());
+                Assert.That(distance, Is.EqualTo(1));
+            }
+
+            {
+                var distance = DynamicTimeWarping.GetDistance(ys, xs, new IntAbs());
+                Assert.That(distance, Is.EqualTo(1));
+            }
+        }
+
         [TestCase("a", "b", 1)]
         [TestCase("abc", "def", 3)]
         [TestCase("abcdef", "azced", 3)]
@@ -18,10 +52,17 @@ namespace NumFlatTest.TimeSeriesTests
         [TestCase("Saturday", "Sunday", 3)]
         [TestCase("distance", "editing", 5)]
         [TestCase("abcdefg", "acdfg", 2)]
-        public void Compute(string x, string y, double expected)
+        public void Levenshtein(string x, string y, double expected)
         {
-            var distance = DynamicTimeWarping.GetDistance(x.ToArray(), y.ToArray(), new Levenshtein());
-            Assert.That(distance, Is.EqualTo(expected));
+            {
+                var distance = DynamicTimeWarping.GetDistance(x.ToArray(), y.ToArray(), new Levenshtein());
+                Assert.That(distance, Is.EqualTo(expected));
+            }
+
+            {
+                var distance = DynamicTimeWarping.GetDistance(y.ToArray(), x.ToArray(), new Levenshtein());
+                Assert.That(distance, Is.EqualTo(expected));
+            }
         }
     }
 
@@ -30,6 +71,14 @@ namespace NumFlatTest.TimeSeriesTests
         public double GetDistance(char x, char y)
         {
             return x == y ? 0 : 1;
+        }
+    }
+
+    class IntAbs : IDistance<int, int>
+    {
+        public double GetDistance(int x, int y)
+        {
+            return Math.Abs(x - y);
         }
     }
 }
