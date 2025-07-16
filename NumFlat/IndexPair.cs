f41440f159794baa051321c4534b69df18bb00b4
Nobuaki Tanaka <sinshu.d@gmail.com>
2025-07-16 18:31:13 +0900

    Revise code structure.

---
+++ NumFlat/IndexPair.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumFlat
+{
+    /// <summary>
+    /// Represents a pair of indices.
+    /// </summary>
+    public struct IndexPair
+    {
+        private int first;
+        private int second;
+
+        internal IndexPair(int first, int second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override string ToString()
+        {
+            return "(" + first + ", " + second + ")";
+        }
+
+        /// <summary>
+        /// The first index.
+        /// </summary>
+        public int First => first;
+
+        /// <summary>
+        /// The second index.
+        /// </summary>
+        public int Second => second;
+    }
+}
--- NumFlat/TimeSeries/SubsequenceDynamicTimeWarping.cs
+++ NumFlat/TimeSeries/SubsequenceDynamicTimeWarping.cs
@@ -62,40 +62,5 @@ namespace NumFlat.TimeSeries
             var min = (a < b) ? a : b;
             return (min < c) ? min : c;
         }
-
-
-
-        /// <summary>
-        /// Represents a pair of indices.
-        /// </summary>
-        public struct IndexPair
-        {
-            private int first;
-            private int second;
-
-            internal IndexPair(int first, int second)
-            {
-                this.first = first;
-                this.second = second;
-            }
-
-            /// <summary>
-            /// <inheritdoc/>
-            /// </summary>
-            public override string ToString()
-            {
-                return "(" + first + ", " + second + ")";
-            }
-
-            /// <summary>
-            /// The first index.
-            /// </summary>
-            public int First => first;
-
-            /// <summary>
-            /// The second index.
-            /// </summary>
-            public int Second => second;
-        }
     }
 }
