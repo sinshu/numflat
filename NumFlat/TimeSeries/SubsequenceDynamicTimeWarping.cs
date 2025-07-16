a516c8a73a376d28d8ddde161260d8f2de6d9b5f
Nobuaki Tanaka <sinshu.d@gmail.com>
2025-07-16 18:20:57 +0900

    Try sDTW.

---
+++ NumFlat/TimeSeries/SubsequenceDynamicTimeWarping.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumFlat.TimeSeries
+{
+    /// <summary>
+    /// Provides DTW (dynamic time warping), a sequence-to-sequence matching algorithm.
+    /// </summary>
+    public static class SubsequenceDynamicTimeWarping
+    {
+        public static Mat<double> ComputeCostMatrix<T, U>(IReadOnlyList<T> xs, IReadOnlyList<U> ys, DistanceMetric<T, U> dm)
+        {
+            var n = xs.Count;
+            var m = ys.Count;
+
+            var dtw = new Mat<double>(n + 1, m + 1);
+            dtw[1.., ..].Fill(double.PositiveInfinity);
+
+            var fdtw = dtw.GetUnsafeFastIndexer();
+            for (var i = 1; i <= n; i++)
+            {
+                for (var j = 1; j <= m; j++)
+                {
+                    var cost = dm(xs[i - 1], ys[j - 1]);
+                    fdtw[i, j] = cost + Min(
+                        fdtw[i - 1, j],      // insertion
+                        fdtw[i, j - 1],      // deletion
+                        fdtw[i - 1, j - 1]); // match
+                }
+            }
+
+            return dtw[1.., 1..];
+        }
+
+        private static double Min(double a, double b, double c)
+        {
+            var min = (a < b) ? a : b;
+            return (min < c) ? min : c;
+        }
+
+
+
+        /// <summary>
+        /// Represents a pair of indices.
+        /// </summary>
+        public struct IndexPair
+        {
+            private int first;
+            private int second;
+
+            internal IndexPair(int first, int second)
+            {
+                this.first = first;
+                this.second = second;
+            }
+
+            /// <summary>
+            /// <inheritdoc/>
+            /// </summary>
+            public override string ToString()
+            {
+                return "(" + first + ", " + second + ")";
+            }
+
+            /// <summary>
+            /// The first index.
+            /// </summary>
+            public int First => first;
+
+            /// <summary>
+            /// The second index.
+            /// </summary>
+            public int Second => second;
+        }
+    }
+}
--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -11,46 +11,15 @@ using NumFlat.Distributions;
 using NumFlat.IO;
 using NumFlat.MultivariateAnalyses;
 using NumFlat.SignalProcessing;
+using NumFlat.TimeSeries;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var positions = new List<Vec<double>>();
-        var random = new Random(42);
-        var n = 20;
-
-        for (var i = 0; i < n; i++)
-        {
-            positions.Add([random.NextGaussian(), random.NextGaussian()]);
-        }
-
-        var d = new Mat<double>(n, n);
-        for (var i = 0; i < n; i++)
-        {
-            for (var j = 0; j < n; j++)
-            {
-                d[i, j] = positions[i].Distance(positions[j]);
-            }
-        }
-
-        var x = ClassicalMultiDimensionalScaling.Fit(d, 2);
-
-        Console.WriteLine(d);
-
-        Console.WriteLine(positions.RowsToMatrix());
-
-        Console.WriteLine(x);
-
-        var d2 = new Mat<double>(n, n);
-        for (var i = 0; i < n; i++)
-        {
-            for (var j = 0; j < n; j++)
-            {
-                d2[i, j] = x.Rows[i][0..2].Distance(x.Rows[j][0..2]);
-            }
-        }
-
-        Console.WriteLine(d2);
+        double[] xs = [1, 2, 3];
+        double[] ys = [0, 0, 0, 0, 1, 2, 3, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 0, 0, 0, 0, 0, 0, 1, 2, 3, 0, 0, 0];
+        var cost = SubsequenceDynamicTimeWarping.ComputeCostMatrix(xs, ys, (x, y) => Math.Abs(x - y));
+        Console.WriteLine(cost);
     }
 }
