477f68b041e485b8a13fd15b1861f23d217710de
Nobuaki Tanaka <sinshu.d@gmail.com>
2026-07-05 11:44:01 +0900

    Try DBA.

--- NumFlat/TimeSeries/DtwBarycenterAveraging.cs
+++ NumFlat/TimeSeries/DtwBarycenterAveraging.cs
@@ -5,20 +5,20 @@ namespace NumFlat.TimeSeries
 {
     public static class DtwBarycenterAveraging
     {
-        public static IReadOnlyList<T> Fit<T>(IReadOnlyList<IReadOnlyList<T>> sequences, DistanceMetric<T, T> dm, DtwBarycenterAveragingOptions options)
+        public static IReadOnlyList<Vec<double>> Fit(IReadOnlyList<IReadOnlyList<Vec<double>>> sequences, DtwBarycenterAveragingOptions options)
         {
             // DTW barycenter averagingを実行して、平均シーケンスを返す
             // optionのmaxIterationsとtoleranceを使用して収束判定を行う
             return null;
         }
 
-        public static IReadOnlyList<T> GetInitialGuess<T>(IReadOnlyList<IReadOnlyList<T>> sequences, DistanceMetric<T, T> dm)
+        public static IReadOnlyList<Vec<double>> GetInitialGuess(IReadOnlyList<IReadOnlyList<Vec<double>>> sequences)
         {
             // DTW距離が最小となるシーケンスを初期推定値として選択する
             return null;
         }
 
-        public static (T[] Sequence, double Error) Update<T>(IReadOnlyList<IReadOnlyList<T>> sequences, DistanceMetric<T, T> dm)
+        public static (Vec<double>[] Sequence, double Error) Update(IReadOnlyList<IReadOnlyList<Vec<double>>> sequences)
         {
             // DTW barycenter averagingのイテレーション一回分を実行
             return (null, 0.0);
