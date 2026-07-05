d89414c26c660f0dfb7ff4a45c3ff7ba625bdfb3
Nobuaki Tanaka <sinshu.d@gmail.com>
2026-07-05 14:59:13 +0900

    Return DBA initial guess without copying

--- NumFlatTest/TimeSeriesTests/DtwBarycenterAveragingTests.cs
+++ NumFlatTest/TimeSeriesTests/DtwBarycenterAveragingTests.cs
@@ -34,20 +34,6 @@ namespace NumFlatTest.TimeSeriesTests
         }
 
         [Test]
-        public void GetInitialGuessSelectsMedoidWithSmallestTotalDtwDistance()
-        {
-            var sourceSequences = ToSequences(
-                [0, 0, 0],
-                [2, 2, 2],
-                [100, 100, 100]);
-
-            var actual = DtwBarycenterAveraging.GetInitialGuess(sourceSequences);
-
-            AssertSequence(actual, [2, 2, 2]);
-            Assert.That(actual, Is.Not.SameAs(sourceSequences[1]));
-        }
-
-        [Test]
         public void UpdateDoesNotIncreaseObjectiveCost()
         {
             var sourceSequences = ToSequences(
